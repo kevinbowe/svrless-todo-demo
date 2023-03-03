3256aa0f193720fcb67ab296218a428fc8825e37
kevinbowe <kevinbowe1957@gmail.com>
2023-03-03 17:47:14 -0500

    Code cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,8 @@
 <script setup lang="ts">
-// Path: src\App.vue
 </script>
 
 <template>
   <v-app id="app">
-    <!-- Add this line: you don't need to import TheAppLayout
-       because it is already registered as explained earlier  -->
     <TheAppLayout />
   </v-app>
 </template>

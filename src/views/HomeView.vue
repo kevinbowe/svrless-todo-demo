1bdf037a01d0483ef7c7cc2f722ed00c23ef5594
kevinbowe <kevinbowe1957@gmail.com>
2023-03-07 10:36:10 -0500

    Add footer to HomeView ---------------------- Add btns. Add company name, copyright, and date. Add footerlinks data to support the buttons. -- The buttons support Home, About, Profile. -- Login and Signup will be added later.
    Fix Toolbar
    --------------------------------
    Fix styling issues with buttons.
    Simplify v-for key values.
    Add missing link to buttons.
    
    Other Stuff
    --------------------------------
    Simple code cleanup in router.ts
    -- No Functional Change

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,21 +4,42 @@
 			<v-toolbar-title>Vuetify-Base-2 Dashboard</v-toolbar-title>
 			<v-spacer></v-spacer>
 			<v-btn
-				v-for="link in links"
-				:key="`${link.label}-header-link`"
-				rounded
+				v-for="link in links" :key=link.label
+				color="white"
+				variant="text"
+				class="mx-2"
+        rounded="xl"
 				:to="link.url"
-				color="secondary"
 			> 
+				{{ link.label }}
 			</v-btn>
 			<v-btn @click="toggleTheme" text rounded>Toggle Theme</v-btn>
 			</v-app-bar>
+
     <v-container class="text-center">
         <h1>Home</h1>
         <v-btn class="pa-3" to="/about">Go To About</v-btn>
         <br />
         <v-btn color="primary" class="ma-3" to="/profile">Go To Profile</v-btn>
     </v-container>
+
+			<v-footer app color="primary" dark fixed>
+      <v-row justify="center" no-gutters>
+        <v-btn
+          v-for="link in footerLinks" :key=link.label
+          color="white"
+          variant="text"
+          class="mx-2"
+          rounded="xl"
+					:to=link.url
+        >
+          {{ link.label }}
+        </v-btn>
+        <v-col class="text-center mt-4" cols="12">
+          <strong>DaBowe.com</strong> &copy {{ new Date().getFullYear() }}
+        </v-col>
+      </v-row>
+    </v-footer>
 </template>
 
 <script lang="ts" setup>
@@ -30,6 +51,11 @@ const links = ref( [
 				{label: 'Home', url: '/' },
 				{label: 'Login', url: '/login'},
 				{label: 'Signup',url: '/signup'}]);
+
+const footerLinks = ref( [
+				{label: 'Home', url: '/' },
+				{label: 'About', url: '/about'},
+				{label: 'Profile',url: '/profile'}]);
 
 const theme = useTheme();
 

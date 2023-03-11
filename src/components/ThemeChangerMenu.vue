cb9f3d712304e636e723449387647abe3c903b52
kevinbowe <kevinbowe1957@gmail.com>
2023-03-11 09:09:19 -0500

    WIP. Getting various pieces of the theme changing code to work. --- The 'avatar' and light/dark toggle have been disabled. --- Added 3x themes for testing. Add [function setTheme()]. It seems to be working. --- There are errors being reported in Chrome and Safari.

--- src/components/ThemeChangerMenu.vue
+++ src/components/ThemeChangerMenu.vue
@@ -1,76 +1,177 @@
 <template>
-	<v-app>
-		<div class="text-center">
- 			<v-menu v-model="menu" :close-on-content-click="false" location="end" >
-				<template v-slot:activator="{ props }" >
-					<v-btn color="indigo" v-bind="props" >
-						<v-icon size="30" color="primary" >mdi-palette</v-icon 	> <!-- DONE -->
-					</v-btn>
-    		</template>
-        <v-card min-width="300">
+  <v-app>
+    <div class="text-center">
+      <v-menu v-model="menu" :close-on-content-click="false" location="end">
+        <template v-slot:activator="{ props }">
+          <v-btn color="indigo" v-bind="props">
+            <v-icon size="30" color="primary">mdi-palette</v-icon>
+          </v-btn>
+        </template>
+
+        <!-- <v-card min-width="300">
           <v-list>
-            <v-list-item
-							prepend-avatar="./src/assets/Beard_DaBowe.svg"
-              title="Da Bowe"
-              subtitle="Mr Bowe if you nasty"
-            >
+            <v-list-item prepend-avatar="./src/assets/Beard_DaBowe.svg" title="Da Bowe" subtitle="Mr Bowe if you nasty">
               <template v-slot:append>
-                <v-btn
-                  variant="text"
-                  :class="fav ? 'text-red' : ''"
-                  icon="mdi-heart"
-                  @click="fav = !fav"
-                ></v-btn>
+                <v-btn variant="text" :class="fav ? 'text-red' : ''" icon="mdi-heart" @click="fav = !fav"></v-btn>
               </template>
             </v-list-item>
           </v-list>
+
           <v-divider></v-divider>
+
           <v-list>
-						<v-list-item>
+            <v-list-item>
               <v-switch @change="toggleDarkMode" :label="`toggle ${switchLabel} mode`" />
-							<!-- <v-switch XX:value="darkMode" @change="toggleDarkMode" :label="`toggle ${switchLabel} mode`" /> -->
             </v-list-item>
-					 <v-divider />
+            <v-divider />
           </v-list>
-  
+
           <v-card-actions>
             <v-spacer></v-spacer>
-  
-            <v-btn
-              variant="text"
-              @click="menu = false"
-            >
-              Cancel
-            </v-btn>
-            <v-btn
-              color="primary"
-              variant="text"
-              @click="menu = false"
-            >
-              Save
-            </v-btn>
+            <v-btn variant="text" @click="menu = false"> Cancel </v-btn>
+            <v-btn color="primary" variant="text" @click="menu = false"> Save </v-btn>
           </v-card-actions>
-        </v-card>
+        </v-card> -->
+
+
+
+
+
+        <v-card-text>
+          <!-- 
+					<v-card class="my-2" :disabled="$vuetify.theme.themes.name === theme.name" @click="setTheme(theme)" hover outlined v-for="(theme, index) in themes" :key="index"> -->
+          <v-card class="my-2" @click="setTheme(myTheme)" v-for="(myTheme, index) in myThemes" :key="index">
+
+						<v-list-item>
+              <v-list-item-content>
+                <v-list-item-title class="font-weight-bold"> {{ myTheme.name }}</v-list-item-title>
+              </v-list-item-content>
+              <v-list-item-action>
+                <v-avatar color="success" size="30" v-if="$vuetify.theme.themes.name === myTheme.name">
+                  <v-icon>mdi-check</v-icon>
+                </v-avatar>
+              </v-list-item-action>
+            </v-list-item>
+            <div class="my-2">
+              <v-chip class="mx-1" label :color="myTheme.dark[key]" v-for="(key, index) in Object.keys(myTheme.dark)" :key="index"> {{ key }}</v-chip>
+            </div>
+            <div class="my-2">
+              <v-chip class="mx-1" label :color="myTheme.light[key]" v-for="(key, index) in Object.keys(myTheme.light)" :key="index"> {{ key }}</v-chip>
+            </div>
+						
+          </v-card>
+        </v-card-text>
       </v-menu>
-	</div>
-</v-app>
+    </div>
+  </v-app>
 </template>
 
 <script lang="ts" setup>
-	import {ref} from 'vue' 
-	import { useTheme } from 'vuetify';
-	import { computed } from "vue";
-	
-	const fav = ref(true);
-	const menu = ref(false);
-	const theme = useTheme();
+import { ref } from "vue";
+import { useTheme } from "vuetify";
+import { computed } from "vue";
 
- 	function toggleDarkMode () {
-		theme.global.name.value = theme.current.value.dark ? 'light' : 'dark';
-	};
+const fav = ref(true);
+const menu = ref(false);
+const uTheme = useTheme();
 
-	const switchLabel = computed( () => {
-		return theme.current.value.dark ? 'Light' : 'Dark'
-	});
+function toggleDarkMode() {
+  uTheme.global.name.value = uTheme.current.value.dark ? "light" : "dark";
+}
+
+const switchLabel = computed(() => {
+  return uTheme.current.value.dark ? "Light" : "Dark";
+});
+
+const myThemes = ref([
+      {
+        name: "theme1",
+        dark: {
+          primary: "#21CFF3",
+          accent: "#FF4081",
+          secondary: "#21dc79",
+          success: "#86af3f",
+          info: "#f34fc6",
+          warning: "#FB8C00",
+          error: "#FF5252"
+        },
+        light: {
+          primary: "#22daff",
+          accent: "#ff6b99",
+          secondary: "#26ff8c",
+          success: "#a5d64c",
+          info: "#ff53d0",
+          warning: "#ff8e00",
+          error: "#ff5252"
+        }
+      },
+      {
+        name: "theme2",
+        dark: {
+          primary: "#E65100",
+          accent: "#7CB342",
+          secondary: "#689F38",
+          success: "#4CAF50",
+          info: "#6156d8",
+          warning: "#1565C0",
+          error: "#FF7043"
+        },
+        light: {
+          primary: "#ffa450",
+          accent: "#a1e754",
+          secondary: "#92de4e",
+          success: "#6dff74",
+          info: "#7365ff",
+          warning: "#2e8ac0",
+          error: "#ff5e3c"
+        }
+      },
+      {
+        name: "theme3",
+        dark: {
+          primary: "#33691E",
+          accent: "#FFCA28",
+          secondary: "#607D8B",
+          success: "#FFEB3B",
+          info: "#2196F3",
+          warning: "#9C27B0",
+          error: "#B71C1C"
+        },
+        light: {
+          primary: "#6ae240",
+          accent: "#ffe063",
+          secondary: "#7ea4b6",
+          success: "#ffea70",
+          info: "#229eff",
+          warning: "#e239ff",
+          error: "#e82424"
+        }
+      }
+    ]);
+
+function setTheme(myTheme) {
+	console.log('myTheme.name == '+myTheme.name);
+	uTheme.global.name.value = myTheme.name;
+
+	// close menu
+	// const menu = false;
+	// const name =ref(theme.name);		
+	// const dark = ref(theme.dark);	
+	// const light = ref(theme.light);
+
+	// Set themes
+	// Object.keys(dark).forEach(i => {
+  // 	$vuetify.theme.themes.dark[i] = dark[i];
+  // });
+
+  // Object.keys(light).forEach(i => {
+  //   $vuetify.theme.themes.light[i] = light[i];
+  // });
+
+	// also save theme name to disable selection
+	// $vuetify.theme.themes.name = name;
+
+	console.log("<-- fini setTheme")
+}
 
 </script>

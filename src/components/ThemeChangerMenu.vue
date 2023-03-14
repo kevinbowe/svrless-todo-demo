3141dbcf9e5b9921d9b3c943c26e9fee14bd3d1c
kevinbowe <kevinbowe1957@gmail.com>
2023-03-14 14:19:30 -0400

    Pre-Cleanup Commit

--- src/components/ThemeChangerMenu.vue
+++ src/components/ThemeChangerMenu.vue
@@ -9,32 +9,50 @@
 				</template>
 				<v-card-text>
 					<!-- <v-card class="my-2" :disabled="$vuetify.theme.themes.name === theme.name" @click="setTheme(theme)" hover outlined v-for="(theme, index) in themes" :key="index"> -->
-					<v-card class="my-2" @click="setTheme(myTheme)" v-for="(myTheme, index) in myThemes" :key="index" >
+					<!-- <v-card class="my-2" @click="setTheme(myTheme)" v-for="(myTheme, index) in myThemes" :key="index" > -->
+					<!-- <v-card class="my-2" @click="readAllThemes(uTheme)" v-for="(myTheme, index) in myThemes" :key="index" > -->
+
+					<!-- 	const themes = Object.entries(uTheme.computedThemes.value); -->
+					
+					<!-- <v-card class="my-2" @click="readAllThemes(uTheme)" v-for="(myTheme, index) in Object.keys(uTheme.computedThemes.value)" :key="index" > -->
+					<v-card class="my-2" v-for="(eachThemeName, themeIndex) in Object.keys(uTheme.computedThemes.value)" :key="themeIndex" >
 						<v-list-item>
+							<!-- Display Theme Name -->
 							<v-list-item-title class="font-weight-bold">
-								{{ myTheme.name }}
+								Theme Name (eachThemeName) -- [{{ eachThemeName }}]&nbsp&nbsp | &nbsp&nbsp Index -- [{{ themeIndex }}]
 							</v-list-item-title>
+							<!-- Display Checkmark when Theme is selected/active -->
 							<v-list-item-action>
-								<v-avatar v-if="uTheme.global.name.value === myTheme.name" 
+								<v-avatar v-if="uTheme.global.name.value === eachThemeName" 
 									color="success" size="30">
 										<v-icon>mdi-check</v-icon>
 								</v-avatar>
 							</v-list-item-action>
 						</v-list-item>
 
+						<!-- DEBUG BUTTON --> <div class="my-2"> <v-chip @click=loadAllColorsXXX(themeIndex) class="mx-1" label> Top Row </v-chip> </div>
+						
+						<!-- Display each color in a specific Theme 
+						     BAD -- Displays the unique Theme and Color-Hex but is missing the Name of the Color -->
 						<div class="my-2">
-							<v-chip v-for="(key, index) in Object.keys(myTheme.dark)" :key="index" 
-								:color="key" class="mx-1" label >
-									{{ key }}
+							<v-chip v-for="(colorCode,colorName) in Object.entries(uTheme.computedThemes.value)[themeIndex][1].colors" colorName="colors"
+								:color="colorCode" class="mx-1" label 
+							>
+								{{ colorName }}<br>
+								{{ colorCode }}
 							</v-chip>
 						</div>
 
-						<div class="my-2">
-							<v-chip  v-for="(key, index) in Object.keys(myTheme.light)" :key="index"
-								:color="key" class="mx-1" label >
-									{{ key }}
+						<!-- BAD -- Display each color in a specific Theme 
+						BAD -- Displays the SAME colors from each Theme -->
+						<!--<div class="my-2">
+							<v-chip v-for="(key) in loadAllColors()"
+								:color="key[1]" class="mx-1" label 
+							>
+								{{ key[0] }}
+								{{ key[1] }}
 							</v-chip>
-						</div>
+						</div> -->
 
 					</v-card>
 				</v-card-text>
@@ -47,116 +65,82 @@
 import { ref } from "vue";
 import { useTheme } from "vuetify";
 import { computed } from "vue";
-
-const fav = ref(true);
 const menu = ref(false);
 const uTheme = useTheme();
 
-function toggleDarkMode() {
-	uTheme.global.name.value = uTheme.current.value.dark ? "light" : "dark";
-}
+function loadAllColorsXXX(themeIndex: number  ) {
+	// console.log("Passed index")
+	// 	console.log(themeIndex)
+	// console.log("Computed Theme at index [" + themeIndex + "]")
+	// 	console.log(Object.entries(uTheme.computedThemes.value)[themeIndex])
+	// console.log("Computed Theme Color Properties at index [" + themeIndex + "][1].colors")
+	// 	console.log(Object.entries(uTheme.computedThemes.value)[themeIndex][1].colors)
+	const themes = Object.entries(uTheme.computedThemes.value);
+	// const targetTheme = themes[themeIndex]
+	// console.log("targetTheme")
+	// console.log(targetTheme);
 
-const switchLabel = computed(() => {
-	return uTheme.current.value.dark ? "Light" : "Dark";
-});
 
-const myThemes = ref([
-	{
-		name: "theme1",
-		dark: {
-			primary: "#21CFF3",
-			accent: "#FF4081",
-			secondary: "#21dc79",
-			success: "#86af3f",
-			info: "#f34fc6",
-			warning: "#FB8C00",
-			error: "#FF5252",
-		},
-		light: {
-			primary: "#22daff",
-			accent: "#ff6b99",
-			secondary: "#26ff8c",
-			success: "#a5d64c",
-			info: "#ff53d0",
-			warning: "#ff8e00",
-			error: "#ff5252",
-		},
-	},
-	{
-		name: "theme2",
-		dark: {
-			primary: "#E65100",
-			accent: "#7CB342",
-			secondary: "#689F38",
-			success: "#4CAF50",
-			info: "#6156d8",
-			warning: "#1565C0",
-			error: "#FF7043",
-		},
-		light: {
-			primary: "#ffa450",
-			accent: "#a1e754",
-			secondary: "#92de4e",
-			success: "#6dff74",
-			info: "#7365ff",
-			warning: "#2e8ac0",
-			error: "#ff5e3c",
-		},
-	},
-	{
-		name: "theme3-Fails",
-		dark: {
-			primary: "#33691E",
-			accent: "#FFCA28",
-			secondary: "#607D8B",
-			success: "#FFEB3B",
-			info: "#2196F3",
-			warning: "#9C27B0",
-			error: "#B71C1C",
-		},
-		light: {
-			primary: "#6ae240",
-			accent: "#ffe063",
-			secondary: "#7ea4b6",
-			success: "#ffea70",
-			info: "#229eff",
-			warning: "#e239ff",
-			error: "#e82424",
-		},
-	},
-	{
-		name: "theme2-Copy-Also-Fails",
-		dark: {
-			primary: "#E65100",
-			accent: "#7CB342",
-			secondary: "#689F38",
-			success: "#4CAF50",
-			info: "#6156d8",
-			warning: "#1565C0",
-			error: "#FF7043",
-		},
-		light: {
-			primary: "#ffa450",
-			accent: "#a1e754",
-			secondary: "#92de4e",
-			success: "#6dff74",
-			info: "#7365ff",
-			warning: "#2e8ac0",
-			error: "#ff5e3c",
-		},
-	},
-]);
+	//for (var themeForIndex in themes) {
+		// console.log("Current Theme ForIndex"); 
+		// // console.log(themeForIndex)
+		// console.log(themeIndex)
 
-function setTheme(myTheme: { name: string; }) {
-	console.log("myTheme.name == " + myTheme.name);
-	var stat = uTheme.current.value.dark ? "light" : "dark"
-	console.log("Enter --> Dark value == " + stat)
+		// [tt] contains all property names in [colors] object
+		// var tt = themes[themeForIndex][1].colors;
+		var tt = themes[themeIndex][1].colors;
+		// console.log("themes[themeForIndex][1].colors"); console.log(tt)
 	
-	uTheme.global.name.value = myTheme.name;
+		// Display all color names in the theme.
+		for(var i=0; i < Object.entries(themes[themeIndex][1].colors).length; i++ ) {
+			console.log("Color-Name: [" + Object.entries(tt)[i][0] + "] == Code: [" + Object.entries(tt)[i][1] + "]")
+		}
+		
+		console.log(themes[themeIndex][0]);
 
-	stat = uTheme.current.value.dark ? "light" : "dark"
-	console.log("Exit <-- Dark value == " + stat)
-	console.log("<-- fini setTheme");
-
+	//}
 }
+
+function loadAllColors() {
+	// This version repeats the SAME colors from each Theme.
+	const themes = Object.entries(uTheme.computedThemes.value);
+	var colors = null;
+	for (var theme in themes) {
+		//console.log(`Theme Name == ` + themes[theme][0])
+
+		// [tt] contains all property names in [colors] object
+		var tt = Object.entries(uTheme.computedThemes.value)[theme][1].colors;
+
+		// Display all color names in the theme.
+		colors = Object.entries(tt);
+		//  for(var i=0; i < colors.length; i++ ) {
+		//  	console.log("Color-Name: [" + colors[i][0] + "] == Code: [" + colors[i][1] + "]")
+	 	// }
+	}
+	return colors;
+}
+
+// EXPERIMENT
+// Fetch each theme
+// Report the names
+function readAllThemes(uThemes: object ) {
+	const themes = Object.entries(uTheme.computedThemes.value);
+	for (var theme in themes) {
+		console.log("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+		console.log(`Theme Name == ` + themes[theme][0])
+
+		// [tt] contains all property names in [colors] object
+		var tt = Object.entries(uTheme.computedThemes.value)[theme][1].colors;
+
+		// Display all color names in the theme.
+		var colors = Object.entries(tt);
+		for(var i=0; i < colors.length; i++ ) {
+			console.log("Color-Name: [" + colors[i][0] + "] == Code: [" + colors[i][1] + "]")
+	 	}
+	}
+}
+// EXPERIMENT
+function setTheme(myTheme: string ) { uTheme.global.name.value = myTheme }
+// EXPERIMENT
+function getAllThemes(){ var keys = Object.entries(uTheme.global.current.value.colors);};
 </script>
--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -6,42 +6,89 @@ import * as directives from 'vuetify/directives';
 import '@mdi/font/css/materialdesignicons.css'
 import { createVuetify, ThemeDefinition } from 'vuetify';
 
-const theme1: ThemeDefinition = {
+const theme10Light: ThemeDefinition = {
 	dark: false,
 	colors: {
 		primary:'#F4D03F',
-		xxbackground: '#B71C1C',
-		xxsurface: '#000',
-		xxsecondary: '#F4D03F',
-		xxerror: '#B00020',
-		xxinfo: '#2196F3',
-		xxsuccess: '#4CAF50',
-		xxwarning: '#FB8C00',
+		background: '#B71C1C',
+		surface: '#000',
+		secondary: '#F4D03F',
+		error: '#B00020',
+		info: '#2196F3',
+		success: '#4CAF50',
+		warning: '#FB8C00',
 	}
 }
-const theme2: ThemeDefinition = {
-	dark: true,
-	colors: {
-		primary:'#E74C3C',
-		xxbackground: '#FF9800', 
-		xxsurface: '#000000',
-		xxsecondary: '#8E44AD',
-		xxerror: '#B00020',
-		xxinfo: '#2196F3',
-		xxsuccess: '#4CAF50',
-		xxwarning: '#FB8C00',
-	},
-}
+// const themeSAFElight: ThemeDefinition = {
+// 	dark: false,
+// 	colors: {
+// 		primary:'#F4D03F',
+// 		background: '#B71C1C',
+// 		surface: '#000',
+// 		secondary: '#F4D03F',
+// 		error: '#B00020',
+// 		info: '#2196F3',
+// 		success: '#4CAF50',
+// 		warning: '#FB8C00',
+// 	}
+// }
 
-const vuetify = createVuetify({
-  components,
-  directives,
-		theme: {
+// const theme1Light: ThemeDefinition = {
+// 	dark: false,
+// 	colors: {
+// 		primary:'#F4D03F',
+// 		background: '#B71C1C',
+// 		surface: '#000',
+// 		secondary: '#F4D03F',
+// 		error: '#B00020',
+// 		info: '#2196F3',
+// 		success: '#4CAF50',
+// 		warning: '#FB8C00',
+// 	}
+// }
+
+// const theme2Dark: ThemeDefinition = {
+// 	dark: true,
+// 	colors: {
+// 		primary:'#E74C3C',
+// 		background: '#FF9800', 
+// 		surface: '#000000',
+// 		secondary: '#8E44AD',
+// 		error: '#B00020',
+// 		info: '#2196F3',
+// 		success: '#4CAF50',
+// 		warning: '#FB8C00',
+// 	}
+// }
+
+// const theme3Dark: ThemeDefinition = {
+// 	dark:true,
+// 	colors: {
+// 		primary: "#33691E",
+// 		accent: "#FFCA28",
+// 		secondary: "#607D8B",
+// 		success: "#FFEB3B",
+// 		info: "#2196F3",
+// 		warning: "#9C27B0",
+// 		error: "#B71C1C",
+// 	}
+// }
+
+
+
+// const vuetify = createVuetify({
+export default createVuetify({
+	components,
+	directives,
+	theme: {
 		defaultTheme: 'light',
 		themes: {
-			theme1,theme2
+			theme10Light,
+			// theme1Light,
+			// theme2Dark,
+			// theme3Dark
 		}
 	}
 });
 
-export default vuetify;
+// export default vuetify;

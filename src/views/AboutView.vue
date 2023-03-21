df950aedb64ca476631e7f3ede3ff4435f1eb53d
kevinbowe <kevinbowe1957@gmail.com>
2023-03-21 16:00:01 -0400

    Moved the "DaBowe" card from MasterLayout to AboutView.

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,27 @@
 <template>
-  <v-container class="text-center">
-    <h1 class="text-primary">About Us Page Content</h1>
-    <v-btn class="pa-3" to="/">Go To Home</v-btn>
-  </v-container>
+<v-app>
+
+	<h1 class="text-primary">About Us Page Content</h1>
+	<v-btn class="pa-3 ma-5" to="/">Go To Home</v-btn>
+	<v-row justify="center" >
+		<v-col  sm="5" >
+			<v-card style="text-align:left;">
+				<v-card-title class="bg-cyan-darken-1 " >
+					<span class="white-text">Da Bowe</span>
+				</v-card-title>
+				<v-list>
+					<v-list-item prepend-avatar="./src/assets/Beard_DaBowe.svg" subtitle="Mr Bowe if yo' nasty">
+						<v-btn variant="text" :class="fav ? 'text-red' : ''" icon="mdi-heart" @click="fav = !fav"></v-btn>
+					</v-list-item>
+				</v-list>
+			</v-card>
+		</v-col>
+	</v-row>
+	
+</v-app>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { ref } from "vue";
+const fav = ref(true);
+</script>

72e0f4b4ed719fa7d130b3805243e58b72b4827b
kevinbowe <kevinbowe1957@gmail.com>
2023-04-04 14:17:59 -0400

    Update DevView.vue to display all major iterations of ThemePreview.

--- src/views/DevView.vue
+++ src/views/DevView.vue
@@ -1,22 +1,25 @@
 <template>
 <v-app>
-	<v-btn class="pa-3 ma-5" to="/">Go To Home</v-btn>
-	<h1 class="text-primary">Dev View Content</h1>
-	<v-container class="text-center">
-		<ThemePreview />
-	</v-container>
+	
+<v-btn class="pa-3 ma-5" to="/">Go To Home</v-btn>
+<h1 class="text-primary">Dev View Content</h1>
+
+<v-container class="text-center">
+	<hr style="height:1em;background:black;">
+	<ThemePreview />
+	<hr style="height:1em;background:black;">
+	<ThemePreview_Exp_v0 />
+	<hr style="height:1em;background:black;">
+	<ThemePreview_Exp_v1 />
+</v-container>
+
 </v-app>
 </template>
 
-<script lang="ts" setup></script>
-
-<script lang="ts">
+<script lang="ts" setup>
+// Experiments
+import ThemePreview_Exp_v0 from "../components/ThemePreview_Exp_v0.vue";
+import ThemePreview_Exp_v1 from "../components/ThemePreview_Exp_v1.vue";
+// Latest version
 import ThemePreview from "../components/ThemePreview.vue";
-
-export default {
-    name: "MasterLayout",
-    components: { 
-		ThemePreview
-	}
-};
 </script>

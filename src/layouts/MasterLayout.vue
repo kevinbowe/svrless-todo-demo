ba3d888a9a65df1d553772249008fec96ee38cf7
kevinbowe <kevinbowe1957@gmail.com>
2023-04-04 14:21:28 -0400

    Move the mdi-palette to the far left of the app-bar. -- Merge <script> into <script setup> and delete <script>.  -- Delete obs and unreferenced functions.

--- src/layouts/MasterLayout.vue
+++ src/layouts/MasterLayout.vue
@@ -5,7 +5,20 @@
         <v-app-bar-nav-icon to="/" icon="mdi-home" size="30"></v-app-bar-nav-icon>
         <v-toolbar-title>Vue Multi Themes (v16)</v-toolbar-title>
         <v-spacer></v-spacer>
-				
+		  <v-btn variant="plain">
+					<v-tooltip activator="parent" location="bottom">Toggle Theme</v-tooltip>
+					<v-icon color="secondary" icon="mdi-palette" size="30" />
+					<v-menu v-model="menu" activator="parent" :close-on-content-click="false" location="end">
+						<v-card>
+							<v-card-actions>
+								<v-spacer></v-spacer>
+								<v-btn variant="text" @click="menu = false">Cancel</v-btn>
+								<v-btn color="primary" variant="text" @click="menu = false">Save</v-btn>
+							</v-card-actions>
+							<ThemeChanger />
+						</v-card>
+					</v-menu>
+				</v-btn>
 				<v-btn color="white" variant="plain" class="mx-2" rounded="xl" to="/dev" >
 					Dev
 				</v-btn>
@@ -43,33 +56,6 @@
 				<v-btn color="white" variant="plain" class="mx-2" rounded="xl" to="/about" >
 					About Us
 				</v-btn>
-
-				<v-btn variant="plain">
-					<v-tooltip activator="parent" location="bottom">Toggle Theme</v-tooltip>
-					<v-icon color="secondary" icon="mdi-palette" size="30" />
-					<v-menu v-model="menu" activator="parent" :close-on-content-click="false" location="end">
-						<v-card min-width="300">
-							<v-card-actions>
-								<v-spacer></v-spacer>
-								<v-btn variant="text" @click="menu = false">Cancel</v-btn>
-								<v-btn color="primary" variant="text" @click="menu = false">Save</v-btn>
-							</v-card-actions>
-							<v-divider></v-divider>
-							<v-list>
-								<v-list-item>
-									<v-switch @change="toggleDarkMode" :label="`toggle ${switchLabel} mode`" />
-								</v-list-item>
-								<v-divider />
-							</v-list>
-
-
-							<!-- Put the Theme Picker below here -->
-							<ThemeChanger />
-
-
-						</v-card>
-					</v-menu>
-				</v-btn>
       </v-app-bar>
 
       <!-- |||||| START Content is insertio |||||| -->
@@ -78,18 +64,9 @@
 
       <v-footer app color="primary" dark fixed>
         <v-row justify="center" no-gutters>
-          <v-btn
-            v-for="link in footerLinks"
-            :key="link.label"
-            color="white"
-            variant="text"
-            class="mx-2"
-            rounded="xl"
-            :to="link.url"
-          >
+				<v-btn v-for="link in footerLinks" :key="link.label" color="white" variant="text" class="mx-2" rounded="xl" :to="link.url" >
             {{ link.label }}
           </v-btn>
-
           <v-col class="text-center mt-4" cols="12">
             <strong>DaBowe.com</strong> &copy {{ new Date().getFullYear() }}
           </v-col>
@@ -99,30 +76,11 @@
   </v-layout>
 </template>
 
-<script lang="ts">
-export default {
-    name: "MasterLayout",
-    components: { 
-		ThemeChanger 
-	}
-};
-</script>
-
-<script lang="ts" setup>
-import { useTheme } from "vuetify";
-import { ref,  computed } from "vue";
+<script setup lang="ts" >
+import { ref } from "vue";
 import ThemeChanger from "../components/ThemeChanger.vue";
 
-const fav = ref(true);
 const menu = ref(false);
-
-function toggleDarkMode() {
-  theme.global.name.value = theme.current.value.dark ? "light" : "dark";
-}
-
-const switchLabel = computed(() => {
-  return theme.current.value.dark ? "Light" : "Dark";
-});
 
 const blogLinks = ref([
   { label: "Blog 1", url: "/blog1" },
@@ -145,9 +103,4 @@ const footerLinks = ref([
   { label: "About Us", url: "/about" },
   { label: "Profile", url: "/profile" },
 ]);
-const theme = useTheme();
-const toggleTheme = () =>
-  (theme.global.name.value = theme.global.current.value.dark
-    ? "light"
-    : "dark");
 </script>

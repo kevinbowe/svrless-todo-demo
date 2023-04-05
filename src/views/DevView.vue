74ff6853fe6c2e5af8663fab611a5f01f263430b
kevinbowe <kevinbowe1957@gmail.com>
2023-04-05 17:44:44 -0400

    Add MasterLayout, ThemeChanger to DevView.vue -- Remove OBS references to ThemePreview

--- src/views/DevView.vue
+++ src/views/DevView.vue
@@ -1,31 +1,15 @@
 <template>
 <v-app>
-	
-<v-btn class="pa-3 ma-5" to="/">Go To Home</v-btn>
-<h1 class="text-primary">Dev View Content</h1>
-
-<v-container class="text-center">
-	<hr style="height:1em;background:black;">
-	<ThemePreview_v2 />
-	<!-- <hr style="height:1em;background:black;"> -->
-	<!-- <ThemePreview_Exp_v2 /> -->
-	<!-- <hr style="height:1em;background:black;"> -->
-	<!-- <ThemePreview_v1 /> -->
-	<!-- <hr style="height:1em;background:black;"> -->
-	<!-- <ThemePreview_Exp_v0 /> -->
-	<!-- <hr style="height:1em;background:black;"> -->
-	<!-- <ThemePreview_Exp_v1 /> -->
-</v-container>
-
+	<MasterLayout>
+	<v-container class="text-center">
+		<h1 class="text-primary">Dev Page Content</h1>
+		<ThemeChanger />
+	</v-container>
+</MasterLayout>
 </v-app>
 </template>
 
 <script lang="ts" setup>
-// Experiments
-import ThemePreview_Exp_v0 from "../components/ThemePreview_Exp_v0.vue";
-import ThemePreview_Exp_v1 from "../components/ThemePreview_Exp_v1.vue";
-import ThemePreview_Exp_v2 from "../components/ThemePreview_Exp_v2.vue";
-// Latest version
-import ThemePreview_v1 from "../components/ThemePreview_v1.vue";
-import ThemePreview_v2 from "../components/ThemePreview_v2.vue";
+import ThemeChanger from "../components/ThemeChanger.vue";
+import MasterLayout from "../layouts/MasterLayout.vue";
 </script>

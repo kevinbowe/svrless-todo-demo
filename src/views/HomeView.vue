349df2951340a806791aa3c3b361c82fd708aeb4
kevinbowe <kevinbowe1957@gmail.com>
2023-04-05 17:43:15 -0400

    Remove references to ThemeChanger in HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,13 +3,11 @@
 <MasterLayout>
 	<v-container class="text-center">
 		<h1 class="text-primary">Home Page Content</h1>
-		<ThemeChanger />
 	</v-container>
 </MasterLayout>
 </v-app>
 </template>
 
 <script setup lang="ts">
-	import ThemeChanger from "../components/ThemeChanger.vue";
 	import MasterLayout from "../layouts/MasterLayout.vue";
 </script>

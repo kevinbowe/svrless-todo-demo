98d8ae987d0266fc88389997ba64146eb91c64f8
kevinbowe <kevinbowe1957@gmail.com>
2023-04-06 08:40:06 -0400

    Change references to MasterLayout / MasterLayout_v1 to MasterLayout_v2. -- No functional change.

--- src/views/DevView.vue
+++ src/views/DevView.vue
@@ -1,15 +1,16 @@
 <template>
 <v-app>
-	<MasterLayout>
+<MasterLayout_v2>
 	<v-container class="text-center">
 		<h1 class="text-primary">Dev Page Content</h1>
 		<ThemeChanger />
 	</v-container>
-</MasterLayout>
+</MasterLayout_v2>
 </v-app>
 </template>
 
 <script lang="ts" setup>
 import ThemeChanger from "../components/ThemeChanger.vue";
-import MasterLayout from "../layouts/MasterLayout.vue";
+import MasterLayout_v1 from "../layouts/MasterLayout_v1.vue";
+import MasterLayout_v2 from "../layouts/MasterLayout_v2.vue";
 </script>

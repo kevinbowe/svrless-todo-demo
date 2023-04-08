01c53faa00db68358b4746824b1c48a2761eb18e
kevinbowe <kevinbowe1957@gmail.com>
2023-04-08 17:57:45 -0400

    New ThemeParts Component: StatusIcons.vue -- This handels displaying the mdi-check or mdi-circle-outline depending on the stat (which is passed to component)

---
+++ src/components/ThemeParts/StatusIcons.vue
@@ -0,0 +1,18 @@
+<template>
+	
+	<v-avatar v-if="stat" color="success" size="30">
+		<v-icon icon="mdi-check"> </v-icon> 
+	</v-avatar>
+	
+	<v-avatar v-else color="grey" size="30">
+		<v-icon icon="mdi-circle-outline"></v-icon> 
+	</v-avatar>
+
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+	stat: Boolean
+})
+
+</script>

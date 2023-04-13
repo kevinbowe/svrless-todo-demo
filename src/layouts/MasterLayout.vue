bc584f01ef6c8cadd2084fc45dbfe4c2c69053ac
kevinbowe <kevinbowe1957@gmail.com>
2023-04-13 08:30:59 -0400

    Rename Workspace -- modify app-bar-title -- No functional change

--- src/layouts/MasterLayout.vue
+++ src/layouts/MasterLayout.vue
@@ -124,7 +124,7 @@
 import { ref } from "vue";
 import ThemeChanger from "../components/ThemeChanger.vue";
 
-const mainTitle: string = "Vue MultiTheme v23c - WIP"
+const mainTitle: string = "Vue MultiTheme v24"
 
 const menu = ref(false);
 

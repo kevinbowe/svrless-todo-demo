8e877232cc690f81fb688cddd09856eff7cc3784
kevinbowe <kevinbowe1957@gmail.com>
2023-04-27 14:45:51 -0400

    Amplify Configuration Adds
    Changes to be committed:
    	new file:   amplify/.config/project-config.json
    	new file:   amplify/README.md
    	new file:   amplify/backend/auth/vueawsauthrawv3ec688e6e/cli-inputs.json
    	new file:   amplify/backend/backend-config.json
    	new file:   amplify/backend/tags.json
    	new file:   amplify/backend/types/amplify-dependent-resources-ref.d.ts
    	new file:   amplify/cli.json
    	new file:   amplify/team-provider-info.json
    	new file:   src/aws-exports.ts

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "vueawsauthrawv3ec688e6e": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}

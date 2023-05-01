56b9f3ab0fb5dc666ce8a59553125f9e2bde45af
kevinbowe <kevinbowe1957@gmail.com>
2023-05-01 16:10:08 -0400

    Changes to be committed: 	modified:   src/views/HomeView.vue ----------------------------------------------------------- Customize Amplify Sign Up, Sign In UI. Display 'Nickname' in 'Hello' message when Sign In is complete. Change order of Sign Up form fields. ----------------------------------------------------------- This commit contains a lot of debug code that will be removed in the next commit.
    HomeView.vue
    -----------------------------------------------------------
    Add Test account emails -- Temporary
    
    Add custom order for form fields with: const formFields = {...}
    -- Requires <authenticator :formFields="..." />
    -- Requires <authenticator-sign-up-form-fields />
    -- Requires import {	AuthenticatorSignUpFormFields } from '@aws-amplify/ui-vue';
    
    Display nickname by accessing user.attributes.nickname

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,15 +4,32 @@
 
 	<v-container class="text-center">
 		<h1 class="text-primary">Home Page Content</h1>
-		kevinbowe1957+0422-230am@gmail.com
+		<hr>
+		<h2 class="text-success">kevinbowe1957+0422-230am@gmail.com</h2>
+		<hr>
+		<h2 class="text-secondary">kevinbowe1957+0501a@gmail.com</h2>
+		<hr>
+		<h2 class="text-primary">kevinbowe1957+0501b@gmail.com </h2>
+		<hr>
+		<h2 class="text-error">kevinbowe1957+0501c@gmail.com </h2>
 	</v-container>
 
-	<authenticator>
+	<authenticator initialState="signIn" :formFields="formFields" :signUpAttributes="['email', 'nickname']">
+		<template v-slot:sign-up-fields>
+			<authenticator-sign-up-form-fields />
+		</template>
+		<template v-slot="{ user, signOut, }">
+			<h1>Hello {{ user.attributes.nickname  }} !</h1>
+			<v-btn color="primary" @click="signOut">Sign Out</v-btn>
+		</template>
+	</authenticator>
+
+	<!-- <authenticator __ORIGINAL__>
 		<template v-slot="{ user, signOut }">
 			<h1>Hello {{ user.username }}!</h1>
 			<v-btn color="primary" @click="signOut">Sign Out</v-btn>
 		</template>
-	</authenticator>
+	</authenticator> -->
 
 	<ThemeChanger />
 
@@ -23,12 +40,78 @@
 <script setup lang="ts">
 	import MasterLayout from "../layouts/MasterLayout.vue";
 	import ThemeChanger from "../components/ThemeChanger.vue";
-
-	import "@aws-amplify/ui-vue/styles.css";
-	import { Amplify } from 'aws-amplify';
+	// import { ref } from "vue";
+	import {	AuthenticatorSignUpFormFields } from '@aws-amplify/ui-vue';
+	import { Amplify, Auth } from 'aws-amplify';
 
 	//	NOTE: aws-exports.js had to be renamed to aws-exports.ts
 	import awsconfig from '../aws-exports';
+	// import { onMounted } from "vue";
+
+	import "@aws-amplify/ui-vue/styles.css";
+
 	Amplify.configure(awsconfig);
+
+	const formFields = {
+    signUp: { 
+		nickname: { order:1 }, email: { order:2 }, 
+		password: { order: 3 }, confirm_password: { order: 4 } }, }
+
+	// const nickname = ref();
+	// async function GetNickname(){	
+	// 	const  info   = await Auth.currentUserInfo();
+	// 	nickname.value = info.attributes.nickname;
+	// 	console.log("currentUserInfo > attributes.nickname ==== ", info.attributes.nickname)
+	// 	// return info.attributes.nickname
+	// }
+
+	// const nickname = ref( {
+	// 	Auth.currentUserInfo().then((info:any) => {
+	// 		return info.attributes.nickname;
+	// 	}) })
+	
+	// const nickname = ref("");
+	
+	// async function GetNickname(){
+	// 		await Auth.currentUserInfo().then((info) => {
+	// 			nickname.value = info.attributes.nickname;
+	// 		});
+	// 	// nickname.value = info.attributes.nickname;
+	// 	//console.log("nickname ==== ", nickname.value)
+	// }
+
+	// onMounted(async () => {
+	// 	try {
+	// 		await Auth.currentUserInfo().then((info) => {
+	// 		nickname.value = info.attributes.nickname;
+	// 	})
+	// 	} catch (err) {}
+	// })
+
+	// const propsXXX = defineProps({
+	// nickname: String, default: () => Auth.currentUserInfo().then((info) => {
+	// 		console.log("Nickname === ",info.attributes.nickname)
+	//  		return info.attributes.nickname;
+	// 	}
+	// })
+
+	// const props = withDefaults(defineProps(), {
+	// 	nickname: () =>
+	// 		Auth.currentUserInfo().then((info) => {
+	// 			console.log("Nickname === ", info.attributes.nickname)
+	// 			return info.attributes.nickname;
+
+	// 		})
+	// })
+
+// 	const nick = computed(() => {
+// 		Auth.currentUserInfo().then((info) => {
+// 			console.log("Nickname === ",info.attributes.nickname)
+// 	 		return info.attributes.nickname;
+// 			// props.nickname? = info.attributes.nickname
+
+// 	 	});
+
+// });
 
 </script>

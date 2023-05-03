19e94d4762e5a7609f3e6eb4eea27afccc020fb5
kevinbowe <kevinbowe1957@gmail.com>
2023-05-03 11:31:14 -0400

    Fix issue where btn:Sign Out is being displayed during Create Account and Confirmation. ----------------------------------------------------------- Modify -- HomeView.vue -- Moved [btn:sign out], Hello 'nickname', Email, and Nickname outside <authentication> scope. -- Simplified the contents on <authenticate> -- Tested 'route' (not routes) for 'authenticated' to control render of [btn]. -- Added 'useAuthenticator' to gain accces to route, user, signout. -- Added toRefs to support useAuthenticator.
    -- Add additional debugging data/code
    -- Removed obs data/code
    -----------------------------------------------------------

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,25 +4,29 @@
 	<v-container class="text-center">
 		<h1 class="text-primary">Home Page Content</h1>
 		<hr>
-		<h2 class="text-success">kevinbowe1957+0422-230am@gmail.com</h2>
-		<hr>
-		<h2 class="text-secondary">kevinbowe1957+0501a@gmail.com</h2>
-		<hr>
-		<h2 class="text-primary">kevinbowe1957+0501b@gmail.com </h2>
-		<hr>
-		<h2 class="text-error">kevinbowe1957+0501c@gmail.com </h2>
+		<h4 class="text-secondary">kevinbowe1957+53a@gmail.com&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp  Kranky  - 53 A  </h4><hr>
+		<h4 class="text-primary">kevinbowe1957+53b@gmail.com&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp    DaBowe  - 53 B  </h4><hr>
+		<h4 class="text-error">kevinbowe1957+53c@gmail.com&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp      Nevik   - 53 C  </h4><hr>
+		<h4 class="text-primary">kevinbowe1957+53d@gmail.com&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp    Mz G    - 53 D  </h4><hr>
+		<h4 class="text-secondary">kevinbowe1957+53e@gmail.com&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp  Chester - 53 E  </h4><hr>
+		<h4 class="text-error">kevinbowe1957+53f@mail.com &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp      KB      - 53 F  </h4><hr>
+		<h4 class="text-black">kevinbowe1957+53g@gmail.com &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp     GitGud  - 53 G  </h4>
+
 	</v-container>
 
 	<authenticator initialState="signIn" :formFields="formFields" :signUpAttributes="['email', 'nickname']">
 		<template v-slot:sign-up-fields>
 			<authenticator-sign-up-form-fields />
 		</template>
-		<template v-slot="{ user, signOut, }">
-			<h1>Hello {{ user.attributes.nickname  }} !</h1>
-			<v-btn color="primary" @click="signOut">Sign Out</v-btn>
-		</template>
 	</authenticator>
 
+	<div v-if="route === 'authenticated'">
+		<h1>Hello {{ user.attributes.nickname  }} !</h1>
+		<div>Email == {{ user.attributes.email  }}</div>
+		<div>NickName == {{ user.attributes.nickname  }}</div>
+		<v-btn v-if="route === 'authenticated'" color="primary" @click="signOut">Sign Out</v-btn>
+	</div>
+	
 	<ThemeChanger />
 
 </MasterLayout>
@@ -32,11 +36,18 @@
 <script setup lang="ts">
 	import MasterLayout from "../layouts/MasterLayout.vue";
 	import ThemeChanger from "../components/ThemeChanger.vue";
-	import {	AuthenticatorSignUpFormFields } from '@aws-amplify/ui-vue';
-	import { Amplify, Auth } from 'aws-amplify';
+	import {	AuthenticatorSignUpFormFields, useAuthenticator } from '@aws-amplify/ui-vue';
+	import { Amplify } from 'aws-amplify';
 	//	NOTE: aws-exports.js had to be renamed to aws-exports.ts
 	import awsconfig from '../aws-exports';
 	import "@aws-amplify/ui-vue/styles.css";
+	import { toRefs } from 'vue'
+	// import {	useAuthenticator } from '@aws-amplify/ui-vue';
+	const { route, user, signOut } = toRefs(useAuthenticator());
+	
+
+
+
 
 	Amplify.configure(awsconfig);
 

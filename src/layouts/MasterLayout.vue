6b3382fb5acfdddb1acb4a24546a55e2f282d065
kevinbowe <kevinbowe1957@gmail.com>
2023-05-10 08:45:43 -0400

    Update App bar title -------------------- modified -- MasterLayout.vue

--- src/layouts/MasterLayout.vue
+++ src/layouts/MasterLayout.vue
@@ -198,7 +198,7 @@ import { ref } from "vue";
 import ThemeChanger from "../components/ThemeChanger.vue";
 import ThemePreview from "../components/ThemePreview.vue";
 // <v-app-bar-title>
-const mainTitle: string = "v3-Auth-v8"
+const mainTitle: string = "v3-Auth-v9"
 
 const menu = ref(false);
 const blogLinks = ref([

0065dc1b0a675c94d65eda15e2cb11bfeeb40e16
kevinbowe <kevinbowe1957@gmail.com>
2023-05-10 17:20:11 -0400

    Update App Bar Title --------------------- Modify -- MasterLayout.vue
    This is a Base-line project folder. -- No changes were made to it beyond the change to the App Bar Title.

--- src/layouts/MasterLayout.vue
+++ src/layouts/MasterLayout.vue
@@ -198,7 +198,7 @@ import { ref } from "vue";
 import ThemeChanger from "../components/ThemeChanger.vue";
 import ThemePreview from "../components/ThemePreview.vue";
 // <v-app-bar-title>
-const mainTitle: string = "v3-Auth-v9"
+const mainTitle: string = "v3-Auth-Form-v0"
 
 const menu = ref(false);
 const blogLinks = ref([

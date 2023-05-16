05b6a392d8dbb80de2b5488d0ab7725a41ed7909
kevinbowe <kevinbowe1957@gmail.com>
2023-05-16 14:42:38 -0400

    Update App Bar Title -- No functional Change ---------------------------------------------- modified -- MasterLayout.vue

--- src/layouts/MasterLayout.vue
+++ src/layouts/MasterLayout.vue
@@ -217,7 +217,7 @@ import { ref } from "vue";
 import ThemeChanger from "../components/ThemeChanger.vue";
 import ThemePreview from "../components/ThemePreview.vue";
 // <v-app-bar-title>
-const mainTitle: string = "v3-Auth-Form-v2"
+const mainTitle: string = "v3-Auth-Form-v4"
 
 const menu = ref(false);
 const devLinks = ref([

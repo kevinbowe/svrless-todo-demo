aef8e778430f6348291313b9c4d36f56225f2ddc
kevinbowe <kevinbowe1957@gmail.com>
2023-05-17 17:54:31 -0400

    Small tweaks to 'enter' and 'exit' console.log commands.

--- src/my-util-code/MyConsoleUtil.ts
+++ src/my-util-code/MyConsoleUtil.ts
@@ -34,11 +34,32 @@ export const progress = (msg: string, value: any = "") => { console.log(`%c${msg
 
 export const joy =     	(msg: string, value: any = "") => { console.log(`%c   ${msg}`, "color: pink;", value) }
 
-export const info =    	(msg: string, value: any = "") => { console.log(`%c   ${msg}`, "color: cyan;", value) }
+export const info =    		(msg: string, value: any = "") => { console.log(`%c   ${msg}`, "color: cyan;", value) }
+export const infor =    	(msg: string, value: any = "") => { console.log(`%c   ${msg}`, "color: red;", value) }
 
-export const enter =    (msg: string, value: any = "") => { console.log(`%cENTER >>>-->  ${msg}`, "color: yellow;", value) }
+export const infob =    	(msg: string, value: any = "") => { console.log(`%c   ${msg}`, "color: ltblue;", value) }
 
-export const exit =    	(msg: string, value: any = "") => { console.log(`%c<--<<< EXIT - ${msg}`, "color: magenta;", value) }
+export const infog =    	(msg: string, value: any = "") => { console.log(`%c   ${msg}`, "color: chartreuse;", value) }
+export const infoy =    	(msg: string, value: any = "") => { console.log(`%c   ${msg}`, "color: yellow;", value) }
+export const infoo =    	(msg: string, value: any = "") => { console.log(`%c   ${msg}`, "color: darkorange;", value) }
+export const infop =    	(msg: string, value: any = "") => { console.log(`%c   ${msg}`, "color: pink;", value) }
+export const infom =    	(msg: string, value: any = "") => { console.log(`%c   ${msg}`, "color: mediumpurple;", value) }
+
+export const info1 =    	(msg: string, value: any = "") => { console.log(`%c   ${msg}`, "color: red;", value) }
+export const info2 =    	(msg: string, value: any = "") => { console.log(`%c   ${msg}`, "color: chartreuse;", value) }
+
+export const info3 =    	(msg: string, value: any = "") => { console.log(`%c   ${msg}`, "color: fuchsia;", value) }
+
+export const info4 =    	(msg: string, value: any = "") => { console.log(`%c   ${msg}`, "color: yellow;", value) }
+export const info5 =    	(msg: string, value: any = "") => { console.log(`%c   ${msg}`, "color: darkorange;", value) }
+export const info6 =    	(msg: string, value: any = "") => { console.log(`%c   ${msg}`, "color: pink;", value) }
+export const info7 =    	(msg: string, value: any = "") => { console.log(`%c   ${msg}`, "color: mediumpurple;", value) }
+
+
+
+export const enter =    (msg: string, value: any = "") => { console.log(`%cENTER >>>-->  ${msg}`, "color: yellow;font-weight:bold;", value) }
+
+export const exit =    	(msg: string, value: any = "") => { console.log(`%c<--<<< EXIT - ${msg}`, "color: green;font-weight:bold;", value) }
 
 export const success = 	(msg: string, value: any = "") => { console.log(`%c${msg}`, "color: green; background-color:black;", value) }
 

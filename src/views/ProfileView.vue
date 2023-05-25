4f562bf19c927a40819dafbb0b644471a4f30164
kevinbowe <kevinbowe1957@gmail.com>
2023-05-25 08:56:07 -0400

    version -- v3-auth-form-update-v5 ----------------------------------------------------------- Add HTML that will display formated 'user.attributes.*' and '*Model' data.
    NOTE
    ----
    The actual data is not available yet.
    
    -----------------------------------------------------------
    Modified -- ProfileView.vue
    ----------------------------------
    Add Template code designed to display
    'user.attributes.[email|nickname]' and '[email|nickname]Model'
    data.
     * This currently fails because the data is not available yet.

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,13 +1,51 @@
 <template>
-<v-app>
-<MasterLayout>
-	<v-container class="text-center">
-		<h1 class="text-primary">Profile Page Content.</h1>
-	</v-container>
-</MasterLayout>
-</v-app>
+	<v-app>
+		<MasterLayout>
+			<v-container class="text-center">
+				<h1 class="text-primary">Profile Page Content.</h1>
+				<v-row>
+					<v-spacer></v-spacer>
+					<v-col cols="4">
+						<v-divider :thickness="20" class="ma-2"></v-divider>
+
+						<v-row>
+							<v-col cols="4" style="text-align:end;">
+								<div>attribute.email</div>
+								<div>attribute.nickname</div>
+							</v-col>
+
+							<v-col cols="6" style="text-align:start;">
+								<div>{{ `{ user.attributes.email }` }}</div>
+								<div>{{ `{ user.attributes.nickname }` }}</div>
+							</v-col>
+						</v-row>
+
+						<v-divider :thickness="20" class="ma-2"></v-divider>
+
+						<v-row>
+							<v-col cols="4" style="text-align:end;">
+								<div>emailModel</div>
+								<div>nicknameModel</div>
+							</v-col>
+
+							<v-col cols="6" style="text-align:start;">
+								<div>{{ `{ emailModel }` }}</div>
+								<div>{{ `{ nicknameModel }` }}</div>
+							</v-col>
+						</v-row>
+
+						<v-divider :thickness="20" class="ma-2"></v-divider>
+
+						<h1>Hello {{ `{ nicknameModel }` }} !</h1>
+
+					</v-col>
+					<v-spacer></v-spacer>
+				</v-row>
+			</v-container>
+		</MasterLayout>
+	</v-app>
 </template>
 
 <script lang="ts" setup>
-	import MasterLayout from "../layouts/MasterLayout.vue";
+import MasterLayout from "../layouts/MasterLayout.vue";
 </script>

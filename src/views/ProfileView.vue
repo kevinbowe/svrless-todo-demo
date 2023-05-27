31fadd9aacc2e7be60b53432998dfdd5de2156ef
kevinbowe <kevinbowe1957@gmail.com>
2023-05-27 17:31:28 -0400

    version -- v3-auth-form-update-v5 ----------------------------------------------------------- Add p1 and p2 props  * Use defineProps()
    Add emailModel and nicknameModel to receive in-coming props.
    --- Note: These parameters are optiona so route.push()
    commands that DONT contain the parameters will not generate
    warnings.
    
    Add minimum required imports
    
    Misc code formatting.
    -----------------------------------------------------------
    modified:   src/views/ProfileView.vue
    -----------------------------------------------------------

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -5,38 +5,27 @@
 				<h1 class="text-primary">Profile Page Content.</h1>
 				<v-row>
 					<v-spacer></v-spacer>
-					<v-col cols="4">
+					<v-col cols="6">
 						<v-divider :thickness="20" class="ma-2"></v-divider>
 
+						<h1>Passed Properties</h1>
 						<v-row>
-							<v-col cols="4" style="text-align:end;">
-								<div>attribute.email</div>
-								<div>attribute.nickname</div>
+							<v-col cols="6" style="text-align:end;">
+								<h2>nicknameModel</h2>
+								<h2>emailModel</h2>
 							</v-col>
 
-							<v-col cols="6" style="text-align:start;">
-								<div>{{ `{ user.attributes.email }` }}</div>
-								<div>{{ `{ user.attributes.nickname }` }}</div>
+							<v-divider vertical :thickness="6" class="my-2" />
+							
+							<v-col cols="4" style="text-align:start;">
+								<h2>{{ nicknameModel }}</h2>
+								<h2>{{ emailModel }}</h2>
 							</v-col>
 						</v-row>
 
 						<v-divider :thickness="20" class="ma-2"></v-divider>
 
-						<v-row>
-							<v-col cols="4" style="text-align:end;">
-								<div>emailModel</div>
-								<div>nicknameModel</div>
-							</v-col>
-
-							<v-col cols="6" style="text-align:start;">
-								<div>{{ `{ emailModel }` }}</div>
-								<div>{{ `{ nicknameModel }` }}</div>
-							</v-col>
-						</v-row>
-
-						<v-divider :thickness="20" class="ma-2"></v-divider>
-
-						<h1>Hello {{ `{ nicknameModel }` }} !</h1>
+						<h1>Hello {{ nicknameModel }} !</h1>
 
 					</v-col>
 					<v-spacer></v-spacer>
@@ -48,4 +37,23 @@
 
 <script lang="ts" setup>
 import MasterLayout from "../layouts/MasterLayout.vue";
+/*  */
+import {
+	info, infor, infog, infob, infoy, infoo, infop, infom,
+	info1, info2, info3, info4, info5, info6, info7,
+	log, warn, err, progress, joy, enter, exit,
+	success, bar, whitebar, fini, start, pass, fail
+}
+	from "../my-util-code/MyConsoleUtil"
+	import { toRefs, ref, Ref, computed } from 'vue'
+
+const props = defineProps({
+	p1: { type: String },
+	p2: { type: String }
+})
+
+const emailModel = ref(props.p1)
+const nicknameModel = ref(props.p2)
+
+
 </script>

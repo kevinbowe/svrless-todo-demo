87c6a3e833c348239c3b82abd9319fc9c703c47a
kevinbowe <kevinbowe1957@gmail.com>
2023-05-29 15:02:50 -0400

    version -- v3-auth-pool-v1     --------------------------     The results on an attempt to remove Auth and import.     The process seemed to fail.     --------------------------     deleted:    amplify/backend/auth/vueawsauthrawv3ec688e6e/cli-inputs.json     modified:   amplify/backend/backend-config.json     modified:   amplify/backend/types/amplify-dependent-resources-ref.d.ts     modified:   amplify/team-provider-info.json

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,13 +1,1 @@
-export type AmplifyDependentResourcesAttributes = {
-  "auth": {
-    "vueawsauthrawv3ec688e6e": {
-      "AppClientID": "string",
-      "AppClientIDWeb": "string",
-      "IdentityPoolId": "string",
-      "IdentityPoolName": "string",
-      "UserPoolArn": "string",
-      "UserPoolId": "string",
-      "UserPoolName": "string"
-    }
-  }
-}
+export type AmplifyDependentResourcesAttributes = {}

9bdb9a58c5e7b69272ae59447d760f8afaca2fc1
kevinbowe <kevinbowe1957@gmail.com>
2023-06-01 12:42:22 -0400

    NEW PROJECT
    version -- v3-auth-pool-v5
    -----------------------------------------------------------
    Add logic to check to see if the user attributes are available.
    Add debug code.
    -----------------------------------------------------------
    modified:   src/views/  ProfileView.vue
    -----------------------------------------------------------

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -39,6 +39,7 @@
 </template>
 
 <script lang="ts" setup>
+start("Enter ProfileView.vue <script setup>")
 import MasterLayout from "../layouts/MasterLayout.vue";
 /*  */
 import {
@@ -60,6 +61,31 @@ const nicknameModel = ref(props.p1)
 const emailModel = ref(props.p2)
 const phone_numberModel = ref(props.p3)
 
+//				Fetch the user attributes if one or more of the 
+//					Parmas/Props/Models are not hydrated.
+let match:any = Object.values(props).find(e => e.length === 0)
+if (match === undefined) {
+	//				If we get here, fetch the users attributes from Cognito
+
+}
 
 
+// //				Report the Params/Properties
+// info(`props.p1 (nickname) == [ ${props.p1} ]`)
+// info2(`props.p2 (email) == [ ${props.p2} ]`)
+// info1(`props.p3 (phone_number) == [ ${props.p3} ]`)
+// //				Report the Models
+// info3(`nicknameModel.value == [ ${nicknameModel.value} ]`/* , nicknameModel */)
+// info5(`emailModel.value == [ ${emailModel.value} ]`/* , emailModel */)
+// info4(`phone_numberModel.value == [ ${phone_numberModel.value} ]`/* , phone_numberModel */)
+
+// //				Check the Params/Props
+// if (props.p1) info("p1 (nicknameModel exist)")
+// 	else info("p1 (nicknameModel does NOT exist)")
+// if (props.p2) info("p2 (emailModel exist)")
+// 	else info("p2 (emailModel does NOT exist)")
+// if (props.p3) info("p3 (phone_numberModel exist)")
+// 	else info("p3 (phone_numberModel does NOT exist)")
+
+fini("Exit ProfileView.vue <script setup>")
 </script>

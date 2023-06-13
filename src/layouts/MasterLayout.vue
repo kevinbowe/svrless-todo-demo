5fa8334aa3e3e4657a0decdfccfb43defbd2a90c
kevinbowe <kevinbowe1957@gmail.com>
2023-06-13 16:16:29 -0400

    NEW PROJECT -- v14
    version -- v3-auth-pool-v14
    --------------------------------------------------
    Update App-Title-Bar -- No functional change.
    --------------------------------------------------
    modified:  src/layouts/  MasterLayout.vue
    --------------------------------------------------

--- src/layouts/MasterLayout.vue
+++ src/layouts/MasterLayout.vue
@@ -220,7 +220,7 @@ import { ref } from "vue";
 import ThemeChanger from "../components/ThemeChanger.vue";
 import ThemePreview from "../components/ThemePreview.vue";
 // <v-app-bar-title>
-const mainTitle: string = "v3-Auth-Pool-v13"
+const mainTitle: string = "v3-Auth-Pool-v14"
 
 const menu = ref(false);
 const devLinks = ref([

1732c67d79945cd000bb072aefaf2c346178ee6b
kevinbowe <kevinbowe1957@gmail.com>
2023-06-23 13:13:11 -0400

    version -- v3-auth-pool-v19 -------------------------------------------------- Fix ISSUE -- username flashes on reload. --- This happens on the Profile page. It is associated with passed parameters.
    Add/Discard misc comments and debug code.
    
    --------------------------------------------------
    modified:    src/views/  ProfileView.vue
    --------------------------------------------------
    
    Modify const usernameModel.
    ____ remove the props.p4 fragment.
    
    ____ This prevents the usernameModel from being initialized too soon during startup.
    
    Modify submitPreferred_username (~) function.
    ____ Remove code that sets preferred_usernameModel.
    
    Replace code that sets the usernameModel based on.
    ____ user.username | props.p4 | user.attributes?.preferred_username.
    
    ____ Effort was made to make this code easy to read/understand.
    
    Add/Discard misc comments and debug code.
    
    --------------------------------------------------

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -166,8 +166,7 @@
 						<v-row no-gutters ><p class="ma-auto">{{ phone_numberModel }}</p></v-row>
 						
 						<v-divider :thickness="3" />
-						<v-row no-gutters style="background-color: rgb(var(--v-theme-surface));"><p class="ma-auto">User Name:</p></v-row>
-						
+						<v-row no-gutters style="background-color: rgb(var(--v-theme-surface));"><p class="ma-auto">User Name:</p></v-row>					
 						<v-row no-gutters >
 							<p class="ma-auto">
 								{{ usernameModel }}
@@ -261,8 +260,7 @@ const EmailConfirmationMessage = { Title: ref(""), Message: ref("") }
 /* Preferred_username / username -- Submit */
 
 const preferred_usernameModel = ref()
-// const preferred_usernameModel = ref(props.p4)
-const usernameModel = ref(props.p4)
+const usernameModel = ref()
 const workingPreferred_usernameModel = ref()
 const resetPreferred_username = () => { workingPreferred_usernameModel.value = preferred_usernameModel.value }
 
@@ -287,7 +285,6 @@ async function submitPreferred_username (event) {
 	await Auth.currentUserInfo().then(result => {
 		//			If we get here, The update worked.
 		usernameModel.value = result.attributes.preferred_username
-		// preferred_usernameModel.value = result.attributes.preferred_username
 	})
 	exit("SUBMIT -- submitPreferred_username")
 }
@@ -655,23 +652,21 @@ let areModelsEmpty = function(){
 	phone_numberModel.value?.length === 0 ||
 	usernameModel.value?.length === 0
 }
-
-info("usernameModel",usernameModel)
-info("preferred_usernameModel", preferred_usernameModel)
-
 async function getSession(){
 	if(areParamsEmpty() || areModelsEmpty()) {
 		//				Check to see if there is an active session.
 		await Auth.currentAuthenticatedUser({bypassCache: true /* false */})
 		.then((user) => {
-			// info(Object.getOwnPropertyNames(user))
-			// info(user.username)
 			emailModel.value = user.attributes?.email
 			emailVerifiedLink.value = user.attributes?.email_verified ? "Verified" : "Not Verified"
 			phone_numberModel.value =  user.attributes?.phone_number
 			nicknameModel.value = user.attributes?.nickname
-			// usernameModel.value = user.username
-			usernameModel.value = user.attributes?.preferred_username ? user.attributes?.preferred_username : user.username
+			//--
+			usernameModel.value = user.username
+			if ( props.p4 ) 
+				usernameModel.value = props.p4
+			if (user.attributes?.preferred_username) 
+				usernameModel.value = user.attributes?.preferred_username
 		})
 		.catch((error) => {
 			//				If I get here, there is no session or params

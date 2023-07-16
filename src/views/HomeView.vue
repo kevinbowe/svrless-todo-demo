0d9622b5606120231937b3099962e412d3354727
kevinbowe <kevinbowe1957@gmail.com>
2023-07-16 10:43:53 -0400

    )))--->    NEW PROJECT -- v6    <---(((
    version -- v3-auth-vtfy3-v6
    --------------------------------------------------
    
    Rename preferred_username validators for consistency.
    
    EXAMPLE
       WAS
    checkShortPreferred_username
       IS
    checkPreferred_usernameTooShort
    
    --------------------------------------------------
    modified:    src/views/  HomeView.vue
    --------------------------------------------------

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -74,9 +74,9 @@
 				<v-form ref="preferred_usernameFormRef" validate-on="submit" @submit.prevent="submitPreferred_username">
 					<v-row>
 						<v-text-field label="User Name" :rules="[
-								checkShortPreferred_username,
-								checkFirstCharPreferred_username,
-								checkSpecialCharExceptionsPreferred_username
+								checkPreferred_usernameTooShort,
+								checkPreferred_usernameFirstChar,
+								checkPreferred_usernameSpecialCharExceptions
 							]" 
 							clearable @click:clear="clearPreferred_usernameModelValidationError"
 							v-model="workingPreferred_usernameModel" hint="Example: kb1" variant="outlined" density="compact" >
@@ -567,21 +567,21 @@ const clearWorkingEmailModelValidationError = () => emailFormRef.value.resetVali
 const clearPreferred_usernameModelValidationError = () => preferred_usernameFormRef.value.resetValidation()
 
 /* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% */
-async function checkShortPreferred_username (workingPreferred_usernameModel) {
+async function checkPreferred_usernameTooShort (workingPreferred_usernameModel) {
 	if (workingPreferred_usernameModel.length > 0 && workingPreferred_usernameModel.length <= 2) {
 		return 'User name is too short. Please try another one.'
 	}
 	return true
 }
 
-async function checkFirstCharPreferred_username (workingPreferred_usernameModel) {
+async function checkPreferred_usernameFirstChar (workingPreferred_usernameModel) {
 		if (!isNaN(workingPreferred_usernameModel[0])) {
 			return 'User name can not begin with a Number. Please try another one.'
 		}
 		return true
 }
 
-async function checkSpecialCharExceptionsPreferred_username (workingPreferred_usernameModel) {
+async function checkPreferred_usernameSpecialCharExceptions (workingPreferred_usernameModel) {
 		//				REF -- ALL Special Chars: /[-_.*\[\]@!#$%^\'"*,:;|/ {}<>()\\]/
 		//				Special Char with exceptions: <hyphen> <under_bar> and <period>
 		// let regexSpecialChar = /[*\[\]@!#$%^\'"*,:;|{}<>()\\\/]/

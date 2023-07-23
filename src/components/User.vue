4c8a429453eb3565559cde70fcd7ff6d23e83293
kevinbowe <kevinbowe1957@gmail.com>
2023-07-23 10:46:25 -0400

    ||||--->    NEW PROJECT -- v8    <---||||
    version -- v3-auth-vtfy3-v8
    --------------------------------------------------
    
    Work-In-Progress -- WIP
    
    Adding validation to the SignUp feature.
    
    Add/Discard misc comments, debug code and formatting.
    
    --------------------------------------------------
    modified:    src/components/   User.vue
    --------------------------------------------------

--- src/components/User.vue
+++ src/components/User.vue
@@ -58,7 +58,18 @@
 									</v-text-field>
 								</v-col>
 								<v-col cols="12">
-									<v-text-field id="emailSuId" v-model="workingEmailModel" clearable density="compact" variant="outlined" label="Email" required />
+									<v-text-field id="emailSuId" v-model="workingEmailModel" 
+
+									ref="workingEmailFieldRef"
+									clearable @click:clear="clearWorkingEmailModelValidationError"
+									:rules="[ 
+										value => checkEmailSpecialChar(value), 
+										value => checkEmailName(value), 
+										value => checkEmailDomain(value),
+									]"
+
+
+									density="compact" variant="outlined" label="Email" required />
 								</v-col>
 								<v-col cols="12">
 									<v-text-field id="usernameSuId" v-model="workingUsernameModel" clearable  density="compact" 
@@ -186,6 +197,15 @@ const openDialogFlag = ref()
 
 const phone_numberModel= ref("")
 
+/*||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+|||                       MOVE THIS CODE WHEN FINISHED	
+|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||| */
+
+/* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% */
+const workingEmailFieldRef = ref()
+const clearWorkingEmailModelValidationError = () => workingEmailFieldRef.value.resetValidation()
+
+
 /* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% */
 /*																											*/
 /**/					const BLOCKAPIFLAG = ref(true)										 /**/
@@ -243,6 +263,89 @@ Hub.listen('auth', (data) => {
 
 /* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% */
 const isCompleteUserSignIn = computed<boolean>(() => workingUsernameModel.value && workingPasswordModel.value ? true : false )
+
+/* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% */
+/* Email -- Validation */
+/* ----------------------------------------------------------------------------- */
+const checkEmailSpecialChar = (emailArg) => {
+					// DEBUG CODE
+					if (emailArg === null) info7("checkEmailSpecialChar(emailArg) > emailArg is Null")
+	
+	//				Check ALL Special Chars -- REFERENCE -- 7/21/23
+	const rxAll = /[+\-_@\.`~!#$%^&'"*,:;/ {}[\]()<>]/gm
+
+	// 			Exclude these Special Chars ---->  +  -  _  @  .
+	const rxExclude = /[`~!#$%^&'"*,:;/ {}[\]()<>]/gm
+	const matchExclude = emailArg.match(rxExclude)
+	if(matchExclude) return `Special chars are not allowed [ ${matchExclude} ]`
+			
+	//				Perform multiple '@'' check
+	const rxMultiAtChar = /@{2}|@.*@/gm
+	const matchMultiAtChar = emailArg.match(rxMultiAtChar)
+	if(matchMultiAtChar) return "Multiple '@' chars are not allowed"
+
+	//				Perform consecutive special char check  ---->  . -  +
+	let rxConsecutive = /\.\.|--|\+\+/gm
+	const matchConsecutive = emailArg.match(rxConsecutive)
+	if ( matchConsecutive) return `Consecutive Special Characters are not allowed. [ ${matchConsecutive} ]`
+	
+	return true
+}
+/* ----------------------------------------------------------------------------- */
+const checkEmailName = (emailArg) => {
+	const parsedEmail = parseEmail(emailArg)
+	if (!parsedEmail) return "FAIL checkEmailName() > Invalid Email"
+	
+	//				Length check ( long & short ) ---->	64 char
+	//				0123456789_123456789_123456789_123456789_123456789_123456789_1234
+	let len = parsedEmail.name.length;
+	if(len > 64) return "FAIL checkEmailName() > Length Check: Max char allowed = 64 char"
+	if(len <= 0) return "FAIL checkEmailName() > Length Check: Min char allowed = 1 char"
+
+	//				Leading and trailing special char check. -- The trailing '_' has been removed from the check.
+	//			 	_asd@gmail.com		-asd@gmail.com		asd-@gmail.com
+	//				+asd@gmail.com		asd+@gmail.com		.asd@gmail.com		asd.@gmail.com
+		const rxLeadTrailChar = /^[-_+\\.]|[-+\\.]$/gm
+	let matchLeadTrailChar = parsedEmail.name.match(rxLeadTrailChar)
+	if(matchLeadTrailChar) return `[-_+.] can not be the first/last char of email name [ ${matchLeadTrailChar} ]`
+
+	return true
+}
+/* ----------------------------------------------------------------------------- */
+const checkEmailDomain = (emailArg) => {
+	const emailDomain = parseEmail(emailArg).domain
+
+	//			Length check ( long & short ) ----> 253 char
+	//			asd@_123456789_123456789_123456789_123456789_123456789_123456789_123456789_123456789_123456789_123456789_123456789_123456789_123456789_123456789_123456789_123456789_123456789_123456789_123456789_123456789_123456789_123456789_123456789_123456789_123456789.com   */
+	let len = emailDomain.length;
+	if(len > 253) return `Max Valid Domain Length: 253 -- Actual Length: ${emailDomain.length}`
+	if(len <= 2) return `Min Valid Domain Length: 3 -- Actual Length: ${emailDomain.length}`
+	
+	//			Split the domain and tld and check from both pieces.
+	const rxDomainAndTLD = /^(?<domain>.*)[\\.|\\s](?<tld>.*)/m
+	let matchDomainAndTLD = emailDomain.match(rxDomainAndTLD)
+
+	//			Check Domain
+	if(matchDomainAndTLD?.groups?.domain.length === 0 || matchDomainAndTLD?.groups?.domain === undefined) 
+		return `An email domain is required ${emailDomain}`
+	
+	//			Check Leading/Trailing Special Char in Domain
+	// 		asd@-asd.com	asd@asd-.com	asd@.asd.com	asd@asd..com
+	// 		asd@_asd.com	asd@asd_.com	asd@+asd.com	asd@asd+.com
+	const rxLeadTrailDomainChar = /^[-_+\\.]|[-_+\\.]$/gm
+	let matchLeadTrailDomainChar = matchDomainAndTLD.groups.domain.match(rxLeadTrailDomainChar)
+	if(matchLeadTrailDomainChar) return `[-_+.] can not be the first/last char in the domain name [ ${matchLeadTrailDomainChar} ]`
+	
+	//			Check	TLD (Top-Level-Domain)
+	if(matchDomainAndTLD?.groups.tld.length === 0 || matchDomainAndTLD?.groups.tld === undefined)
+		return "FAIL checkEmailDomain() > Domain Check: TopLevelDomain is missing"
+	
+	//			Check Leading/Trailing Special Char in TLD
+	let matchLeadAndTrailTldChar = matchDomainAndTLD?.groups.tld.match(rxLeadTrailDomainChar)
+	if(matchLeadAndTrailTldChar) return `[-_+.] can not be the first/last char in the TLD name [ ${matchLeadAndTrailTldChar} ]`
+	return true
+}
+
 
 /* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% */
 const buildUserConfirmationMessage = (email:string|null = null, restartConfirm:Boolean = false) => {

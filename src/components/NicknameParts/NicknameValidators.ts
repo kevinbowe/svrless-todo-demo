b5bd0e6576ac7bcf1fe3579dc18628d2418777f4
kevinbowe <kevinbowe1957@gmail.com>
2023-07-24 07:47:43 -0400

    ||||--->    NEW PROJECT -- v8    <---||||
    version -- v3-auth-vtfy3-v8
    --------------------------------------------------
    
    Renamed Nickname.ts into NicknameValidators.ts
    Enabled all disabled code.
    Discarded Unreferenced and Obsolete code.
    
    --------------------------------------------------
    Added:      src/components/   NicknameParts/   NicknameValidators.ts
    Deleted:    src/components/   NicknameParts/   Nickname.ts
    --------------------------------------------------

---
+++ src/components/NicknameParts/NicknameValidators.ts
@@ -0,0 +1,35 @@
+/* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% */
+/* Validation */
+export async function checkNicknameReserved (workingNickname) {
+	if (workingNickname.toLowerCase() === 'kevin') return 'User nickname reserved. Please try another one.'
+	return true
+}
+export async function checkNicknameTooShort (workingNickname) {
+	if (workingNickname.length <= 2) return 'User nickname is too short. Please try another one.'
+	return true
+}
+export async function checkNicknameNumericFirstChar (workingNickname) {
+	if (!isNaN(workingNickname[0])) return 'User nickname can not begin with a Number. Please try another one.'
+	return true
+}
+export async function checkNicknameFirstChar (workingNickname) {
+	//				All special chars must be rejected.
+	const match = workingNickname[0].match(/[-\._=+`~\\!@#$%\^&*(){}[\]<>?/|]/)
+	if (match) return 'User nickname can not begin with any special characters. Please try another one.'
+	return true
+}
+export async function checkNicknameLastChar (workingNickname) {
+	//				All special chars must be rejected.
+	//		/[-\._=+`~\\!@#$%\^&*(){}[\]<>?|]/
+	const match = workingNickname[workingNickname.length-1].match(/[-\._=+`~\\!@#$%\^&*(){}[\]<>?|]/)
+	if (match) return 'User nickname can not end with any special characters. Please try another one.'
+	return true
+}
+export async function checkNicknameSpecialChars (workingNickname) {
+	//				These chars are valid.
+	//			>>>-->  -  .  _  =  +  ~ <--<<<
+	const match = workingNickname.match( /[`\\!@#$%\^&*(){}[\]<>?/|]/)
+	if(match) return 'User nickname can not contain special characters. Please try another one.'
+	return true
+}
+/* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% */

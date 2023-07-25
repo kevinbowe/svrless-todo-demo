ad5b3995777a6e21e2115f728e5c8a11f80b5232
kevinbowe <kevinbowe1957@gmail.com>
2023-07-25 08:28:09 -0400

    ||||--->    NEW PROJECT -- v8    <---||||
    version -- v3-auth-vtfy3-v8
    --------------------------------------------------
    
    These changes are WIP debug code.
    
    --------------------------------------------------
    modified:   src/components/   UserInfo.vue
    --------------------------------------------------

--- src/components/UserInfo.vue
+++ src/components/UserInfo.vue
@@ -1,41 +1,32 @@
 <template>
 	<!-- User Info -->
-	<v-row justify="center" v-if="isSession">
+	<v-row justify="center">
 		<v-col :lg="4" :md="6" :sm="8" :xs="12" class="ma-auto" >
 			<v-divider :thickness="10" class="ma-2"></v-divider>
 			<v-row no-gutters>
 				<v-col style="background-color: rgb(var(--v-theme-surface)); color: rgb(var(--v-theme-border_alt));">
 					<p>Nick Name:</p></v-col>
-				<v-col>
-					<p>{{ nickname }}</p></v-col>
+				<v-col><p>{{ nicknameModel }}</p></v-col>
 			</v-row>
 			<v-divider :thickness="3" />
 			<v-row no-gutters style="background-color:rgb(var(--v-theme-surface)); color: rgb(var(--v-theme-border_alt));">
 				<p class="ma-auto">Email:</p></v-row>
-			<v-row no-gutters><p class="ma-auto">{{ email }}</p></v-row>
+			<v-row no-gutters><p class="ma-auto">{{ emailModel }}</p></v-row>
 			<v-divider :thickness="3" />
 			<v-row no-gutters style="background-color: rgb(var(--v-theme-surface)); color: rgb(var(--v-theme-border_alt));">
 				<p class="ma-auto">Phone Number:</p></v-row>
-			<v-row no-gutters ><p class="ma-auto">{{ phone_number }}</p></v-row>
+			<v-row no-gutters ><p class="ma-auto">{{ phone_numberModel }}</p></v-row>
 			<v-divider :thickness="3" />
 			<v-row no-gutters style="background-color: rgb(var(--v-theme-surface)); color: rgb(var(--v-theme-border_alt));">
 				<p class="ma-auto">User Name:</p></v-row>
-				<v-row no-gutters ><p class="ma-auto">{{ username }}</p></v-row>
+				<v-row no-gutters ><p class="ma-auto">{{ usernameModel }}</p></v-row>
 			<v-divider :thickness="10"></v-divider>
 		</v-col>
 	</v-row>
 </template>
 
-
 <script setup lang="ts">
-/* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% */
-defineProps({
-	isSession: Boolean,
-	nickname: String,
-	email: String,
-	phone_number:String,
-	username: String
-})
+import { nicknameModel, emailModel, phone_numberModel, usernameModel} from "../components/User.vue"
 
 /* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% */
 </script>

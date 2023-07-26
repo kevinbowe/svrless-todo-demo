ed4bdfcee9bed88ae99d6940d249203413439394
kevinbowe <kevinbowe1957@gmail.com>
2023-07-26 11:54:17 -0400

    ||||--->    NEW PROJECT -- v8    <---||||
    version -- v3-auth-vtfy3-v8
    --------------------------------------------------
    
    Experimental components designed to test emit behavior.
    These components are referenced in HomeView.vue.
    
    These components will be discarded later.
    
    Add/Discard misc comments, debug code and formatting.
    
    --------------------------------------------------
    modified:    src/components/   Experiment_one.vue
    modified:    src/components/   Experiment_two.vue
    --------------------------------------------------

---
+++ src/components/Experiment_one.vue
@@ -0,0 +1,25 @@
+<template>
+	<h1>Experiment One</h1>
+	<v-row no-gutters >
+		<v-spacer/>
+		<v-col cols="8">
+			<v-btn class="mt-3" color="primary" @click="experimentOne">
+				Exp One
+			</v-btn>
+		</v-col>
+		<v-spacer/>
+	</v-row>
+</template>
+
+<script setup lang="ts">
+const emit = defineEmits(['onExperimentEmit'])
+const experimentOne = () => {
+	console.log("Executing --> Experiment_one.vue >  --> experimentOne()")
+	const obj = {
+		isSignedInFlag:true, 
+		username:"Mr_Kranky", 
+		phone:"(919) 272-7866"
+	}
+	emit('onExperimentEmit', obj)
+}
+</script>
---
+++ src/components/Experiment_two.vue
@@ -0,0 +1,24 @@
+<template>
+	<h1>Experiment Two</h1>
+	<v-row no-gutters >
+		<v-spacer/>
+		<v-col cols="8">
+			<v-btn class="mt-3" color="primary" @click="experimentTwo">
+				Exp Two
+			</v-btn>
+		</v-col>
+		<v-spacer/>
+	</v-row>
+</template>
+
+<script setup lang="ts">
+const emit = defineEmits(['onExperimentEmit'])
+const experimentTwo = () => {
+	console.log("Executing --> Experiment_two.vue > experimentTwo()")
+	const obj = {
+		isSignedInFlag:false, 
+		username:"Miz_Glenda"
+	}
+	emit('onExperimentEmit', obj)
+}
+</script>

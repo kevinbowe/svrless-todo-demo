afeab27be2ce9f4ca2324267e511a4795f486bf7
kevinbowe <kevinbowe1957@gmail.com>
2023-08-05 14:31:55 -0400

    version -- v3-auth-vtfy3-v10 --------------------------------------------------
    Cleanup and factor code.
    Try to prevent HTTP Response Error 400 from displaying in console. -- Failed.
    
    --------------------------------------------------
    modified:    src/components/  Preferred_username.vue
    --------------------------------------------------
    
    Rename multiple '*Preferred_username*' const decls to '*username*'.
       Example
       -------
       workingPreferred_usernameModel TO usernameModel
       clearPreferred_usernameModelValidationError TO clearUsernameModelValidationError
       submitPreferred_username(~) TO submitUsername(~)
    
    Modify <v-card> to use <v-card-title> and <v-card-text>
       The title and message now display the errorPopup.title or errorPopup.msg values.
       Removed the height and width attributes.
    
    Modify submitPreferred_username(~)
       Discard all debug code.
       Discard "try/catch" block.
       ___ Use promise ".catch()"
    
    Discard ALL previous, disabled versions of submitPreferred_username(~)
    
    NOTE
    ----
    I was unsuccessful in preventing the Post Error 400 message from displaying in the ChromeDev > Console.
    The exception is being handled but I couldnt hide the Error 400.
    
    --------------------------------------------------

--- src/components/Preferred_username.vue
+++ src/components/Preferred_username.vue
@@ -2,19 +2,19 @@
 	<!-- Update Preferred Username -->
 	<v-row justify="center">
 		<v-col :sm="8" :md="6" :lg="4" class="ma-5" >
-			<v-form ref="preferred_usernameFormRef" validate-on="submit" @submit.prevent="submitPreferred_username">
+			<v-form ref="usernameFormRef" validate-on="submit" @submit.prevent="submitUsername">
 				<v-row>
 					<v-text-field label="User Name" :rules="[
 							value => checkPreferred_usernameTooShort(value),
 							value => checkPreferred_usernameFirstChar(value),
 							value => checkPreferred_usernameSpecialCharExceptions(value)
 						]" 
-						clearable @click:clear="clearPreferred_usernameModelValidationError"
-						v-model="workingPreferred_usernameModel" hint="Example: kb1" variant="outlined" density="compact" >
+						clearable @click:clear="clearUsernameModelValidationError"
+						v-model="usernameModel" hint="Example: kb1" variant="outlined" density="compact" >
 					</v-text-field>
 				</v-row>
 				<v-row class="justify-end">
-					<v-btn :disabled="!workingPreferred_usernameModel" color="primary" type="submit"> Save Preferred Username</v-btn>
+					<v-btn :disabled="!usernameModel" color="primary" type="submit"> Save Preferred Username</v-btn>
 				</v-row>
 			</v-form>
 		</v-col>
@@ -23,15 +23,14 @@
 	<!-- __ERROR_POPUP__ -->
 	<v-row justify="center" v-if="showDialog" >
 		<v-dialog activator="parent" v-model="showDialog" persistent >
-			<v-card 	color="background_alt" border="lg" 
-						class="ma-auto" height="10em" width="20em" elevation="24">
-				<v-card-text> 
-					<h1>Oops!</h1><strong>Something went wrong...</strong>
-				</v-card-text>
+			<v-card color="background_alt" border="lg" class="ma-auto" elevation="24">
+				<v-card-title class="text-h5"> {{ errorPopup.title }}</v-card-title>
+				<v-card-text> {{ errorPopup.msg }}</v-card-text>
 				<v-card-actions>
-					<v-btn @click="showDialog = false" block 
-					color="surface" 
-					style="background-color:rgb(var(--v-theme-primary))"> OK </v-btn>
+					<v-btn @click="showDialog = false" block color="surface" 
+					style="background-color:rgb(var(--v-theme-primary))"> 
+						OK 
+				</v-btn>
 				</v-card-actions>
 			</v-card>
 		</v-dialog>
@@ -44,7 +43,7 @@
 </script>
 
 <script setup lang="ts">
-import { info, info1, info2 , info3, info4, info5, info6, info7 } from "../my-util-code/MyConsoleUtil"
+import { err, info, info1, info2 , info3, info4, info5, info6, info7 } from "../my-util-code/MyConsoleUtil"
 import { enter, enter0, enter1, enter2, enter3, enter4, enter5, enter6, enter7 } from "../my-util-code/MyConsoleUtil"
 import { bar, whitebar, greybar} from "../my-util-code/MyConsoleUtil"
 /* ----------------------------------------------------------------------------- */
@@ -56,163 +55,41 @@ import { checkPreferred_usernameTooShort, checkPreferred_usernameFirstChar,
 	from "../components/Preferred_usernameParts/Preferred_usernameValidators"
 
 /* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% */
-const workingPreferred_usernameModel = ref("")
-const preferred_usernameFormRef = ref()
-const emit = defineEmits()
+const usernameModel = ref("")
+const usernameFormRef = ref()
 
+const emit = defineEmits()
 const showDialog = ref(false)
+const errorPopup = ref({title:"", msg:""})
 
 /* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% */
-async function submitPreferred_username (event) {
-	if(BLOCKAPI("submitPreferred_username function "))return
+async function submitUsername (event) {
+	if(BLOCKAPI("submitUsername function "))return
 	const results = await event
 	if(!results.valid) return 
 	//				If we get here, validation was sucessful
+
 	//				This will return the user in the user pool (not updated )
 	const newuser = await Auth.currentAuthenticatedUser({bypassCache: true});
-						info(`Current Auth User [ ${newuser.username} ] `)
-						info(`Current Auth Preferred_User [ ${newuser.attributes.preferred_username} ] `)
-
-	try {
-	await Auth.updateUserAttributes(newuser, {'preferred_username': workingPreferred_usernameModel.value})
+	await Auth.updateUserAttributes(newuser, {'preferred_username': usernameModel.value})
 	.then(response => {
-						info(`The username is updating`)
-						info(`Response Status ${response.status}`)
-						info1(`Response [ ${response} ]`)
-						info2(`Response [ ${Object.getOwnPropertyNames(response)} ]`)
-						info2(`Response [ ${Object.getOwnPropertyNames(response[1])} ]`)
-		if(!response.OK) {
-			throw new Error("Oops")
-		}
-		Auth.currentUserInfo()
-		.then(result => {
-						info1(`JOY -- This is the CURRENT username-1 > [ ${result.attributes.preferred_username} ]`)
+		//				This code will not execute --IF-- the function above causes an exception.
+		Auth.currentUserInfo().then(result => {
 			//			If we get here, The update worked.
 			emit('onUpdatePreferred_username', { preferred_username: result.attributes.preferred_username})
-			workingPreferred_usernameModel.value = ""
+			usernameModel.value = ""
 		})
 	})
-	// .catch((error) => {
-	// 	//			If I get here, there was a problem updating the preferred_username
-	// 					info(`There was an error...`) //  "AliasExistsException: Already found an entry for the provided username."
-	// 					info1(`   ${error}`) //  "AliasExistsException: Already found an entry for the provided username."
-	// 	Auth.currentUserInfo().then(result => {
-	// 				info2(`FAIL -- This is the Current Username-2 > [ ${result.attributes.preferred_username} ]`)
-	// 		emit('onUpdatePreferred_username', { preferred_username: result.attributes.preferred_username})
-	// 		workingPreferred_usernameModel.value = ""
-	// 	})
-	// 	showDialog.value = true
-	// }) // END_ASYNC_CATCH
-	} catch (e) {
-		info7(`Outside Catch\n      [ ${e} ]`)
-	}
-
+	.catch((error) => {
+		//			If I get here, there was a problem updating the preferred_username
+		errorPopup.value = {title:`Username not available`, msg:`"${usernameModel.value}" is not available.` }
+		usernameModel.value = ""
+		showDialog.value = true
+	}) 
 }
-// /* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% */
-// async function submitPreferred_username (event) {
-// 	if(BLOCKAPI("submitPreferred_username function "))return
-// 	const results = await event
-// 	if(!results.valid) return 
-// 	//				If we get here, validation was sucessful
-// 	//				This will return the user in the user pool (not updated )
-// 	const newuser = await Auth.currentAuthenticatedUser({bypassCache: true});
-// 						info(`Current Auth User [ ${newuser.username} ] `)
-// 						info(`Current Auth Preferred_User [ ${newuser.attributes.preferred_username} ] `)
-// 
-// 	await Auth.updateUserAttributes(newuser, {'preferred_username': workingPreferred_usernameModel.value})
-// 	.then(response => {
-// 						info(`The username is updating`)
-// 		Auth.currentUserInfo()
-// 		.then(result => {
-// 						info1(`JOY -- This is the CURRENT username-1 > [ ${result.attributes.preferred_username} ]`)
-// 			//			If we get here, The update worked.
-// 			emit('onUpdatePreferred_username', { preferred_username: result.attributes.preferred_username})
-// 			workingPreferred_usernameModel.value = ""
-// 		})
-// 	})
-// 	.catch((error) => {
-// 		//			If I get here, there was a problem updating the preferred_username
-// 						info(`There was an error...`) //  "AliasExistsException: Already found an entry for the provided username."
-// 						info1(`   ${error}`) //  "AliasExistsException: Already found an entry for the provided username."
-// 		Auth.currentUserInfo().then(result => {
-// 					info2(`FAIL -- This is the Current Username-2 > [ ${result.attributes.preferred_username} ]`)
-// 			emit('onUpdatePreferred_username', { preferred_username: result.attributes.preferred_username})
-// 			workingPreferred_usernameModel.value = ""
-// 		})
-// 		showDialog.value = true
-// 	})
-// }
-
-// /* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% */
-// async function submitPreferred_username (event) {
-// 	if(BLOCKAPI("submitPreferred_username function "))return
-// 	const results = await event
-// 	if(!results.valid) return 
-// 	//				If we get here, validation was sucessful
-// 	//				This will return the user in the user pool (not updated )
-// 	const newuser = await Auth.currentAuthenticatedUser({bypassCache: true});
-// 
-// 					info(`We got the current user`)
-// 
-// 	await Auth.updateUserAttributes(newuser, {'preferred_username': workingPreferred_usernameModel.value})
-// 	.then(response => {
-// 
-// 					info(`The username has been updated`)
-// 
-// 	})
-// 	.catch((error) => {
-// 		//			If I get here, there was a problem updating the preferred_username
-// 
-// 					info(`There was an error \n ${error}`) //  "AliasExistsException: Already found an entry for the provided username."
-// 
-// 		showDialog.value = true
-// 		return
-// 	})
-// 
-// 					info1(`We caught the error but processing continues `)
-// 
-// 					info2(`Broadcasting the new Preferred Username back to HomeView.`)
-// 	
-// 	await Auth.currentUserInfo()
-// 	.then(result => {
-// 
-// 					info(`This is the CURRENT username [ ${result.attributes.preferred_username} ]`)
-// 
-// 		//			If we get here, The update worked.
-// 		emit('onUpdatePreferred_username', { preferred_username: result.attributes.preferred_username})
-// 		workingPreferred_usernameModel.value = ""
-// 	})
-// }
-
-// /* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% */
-//	//
-// async function submitPreferred_username (event) {
-// 	if(BLOCKAPI("submitPreferred_username function "))return
-// 	const results = await event
-// 	if(!results.valid) return 
-// 	//				If we get here, validation was sucessful
-// 	//				This will return the user in the user pool (not updated )
-// 	const newuser = await Auth.currentAuthenticatedUser({bypassCache: true});
-// 	await Auth.updateUserAttributes(newuser, {
-// 		'preferred_username': workingPreferred_usernameModel.value
-// 	})
-// 	.catch((error) => {
-// 		//			If I get here, there was a problem updating the preferred_username
-// 					info(error) //  "AliasExistsException: Already found an entry for the provided username."
-// 		showDialog.value = true
-// 		return
-// 	})
-// 					info1(`We caught the error but processing continues `)
-// 	await Auth.currentUserInfo().then(result => {
-// 		//			If we get here, The update worked.
-// 		emit('onUpdatePreferred_username', { preferred_username: result.attributes.preferred_username})
-// 		workingPreferred_usernameModel.value = ""
-// 	})
-// }
-
 
 /* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% */
-const clearPreferred_usernameModelValidationError = () => preferred_usernameFormRef.value.resetValidation()
+const clearUsernameModelValidationError = () => usernameFormRef.value.resetValidation()
 
 /* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% */
 /**/					const BLOCKAPIFLAG = ref(false)										 /**/

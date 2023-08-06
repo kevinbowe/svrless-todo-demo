ddce29454bd9cab5d8d17330d40d9ba5211db07a
kevinbowe <kevinbowe1957@gmail.com>
2023-08-06 10:57:58 -0400

    version -- v3-auth-vtfy3-v10 --------------------------------------------------
    Work-In-Progress -- WIP
    
    Initial set of changes for the Change Email > Conf field input field.
    Add popup warnings and validation.
    
    Styling update will be part of the next commit for this feature.
    
    --------------------------------------------------
    added:    src/components/   Email.vue
    --------------------------------------------------
    
    Changes include
    ---------------
    Cancel ( X ) button.
       Clear the working model
    Input Validation ( null, length, Special Char)
       Display validatiom error messages
    Clear working conf input and validation error messages.
    Invalid Code Popup
    On resend code:
       Clear input.
       Clear validation errors.
    
    --------------------------------------------------

--- src/components/Email.vue
+++ src/components/Email.vue
@@ -20,13 +20,17 @@
 		</v-form>
 		</v-col>
 	</v-row>
+
 	<!-- Update Email Confirmation -->
 	<v-row justify="center" >
 		<v-overlay class="align-center justify-center" v-model="toggleConfirmEmail" >
 			<v-sheet height="24em" width="20em" color="surface_alt" elevation="24">
 					<v-row><v-spacer/>
+
 					<v-btn __THIS_IS_THE_X_IN_UPPER_RIGHT__ 
-						class="mr-3" icon="$close" size="large" variant="text" @click="toggleConfirmEmail=false"></v-btn>
+					@click="toggleConfirmEmail=false; workingEmailModel = ''; "
+					class="mr-3" icon="$close" size="large" variant="text" ></v-btn>
+
 				</v-row>
 				<v-col style="margin-top:-2.5em;">
 					<v-row no-gutters>
@@ -35,22 +39,60 @@
 						<p class="ma-auto" v-html="emailConfirmationMessage.Message2.value"></p>
 						<p class="ma-auto" v-html="emailConfirmationMessage.Message3.value"></p>
 					</v-row>
-					<v-row><v-spacer/>
-						<v-col cols="11">
-							<v-text-field label="Confirmation Code" v-model="confirmEmailCodeModel" clearable @click:clear="invalidEmailConfirmCode = ''"
-								id="ConfCode" placeholder="Enter your code" class="mb-2" style="height:1.75em;" variant="outlined"  density="compact">
-							</v-text-field>
-							<p class="mt-4" style="color:rgb(var(--v-theme-error));" >{{ invalidEmailConfirmCode }}</p>
-						</v-col><v-spacer/>
-					</v-row>
-					<v-row class="mx-5">
-						<v-btn :disabled="!confirmEmailCodeModel" @click="applyEmailConfirmationCode" block color="primary" class="mb-2" > Confirm </v-btn>
+					<v-form validate-on="submit" @submit.prevent="applyEmailConfirmationCode">
+						<v-row>
+							<v-spacer/>
+							<v-col cols="11">
+								
+								<v-text-field 
+								_style="height:1.75em;" 
+								label="Confirmation Code" 
+								v-model="confirmEmailCodeModel" 
+								ref="confirmEmailCodeModelFieldRef"
+								id="ConfCode" 
+								:rules="[
+									value => !!value,
+									value => checkConfirmationTooShort(value),
+									value => checkConfirmationSpecialChars(value),
+								]"
+								placeholder="Enter your code" 
+								variant="outlined"  
+								density="compact"
+								class="mb-2" 
+								clearable @click:clear="clearConfirmEmailCodeModelValidationError"
+								/>
+							</v-col>
+							<v-spacer/>
+						</v-row>
+						<v-row class="mx-5 mt-5" _class="mx-5">
+							<v-btn type="submit" :disabled="!confirmEmailCodeModel" block color="primary" class="mb-2" > Confirm </v-btn>
+						</v-row>
+					</v-form>
+					<v-row class="mx-5">	
 						<v-btn block color="surface" class="mb-2" @click="resendEmailConfirmationCode"> Resend Code </v-btn>
 					</v-row>
 				</v-col>
 			</v-sheet>
 		</v-overlay>
 	</v-row>
+
+	<!-- PopUp Message Dialog -- Modal -->
+	<v-row justify="center" v-if="openDialogFlag" >
+		<v-dialog activator="parent" v-model="openDialogFlag" persistent >
+			<v-card 	color="background_alt" border="lg" 
+						class="ma-auto" height="10em" width="20em" elevation="24">
+				<v-card-text> 
+					<h1>Error</h1><strong>Invalid Confirmation Code.</strong>
+				</v-card-text>
+				<v-card-actions>
+					<v-btn @click="openDialogFlag = false" block 
+					color="surface" 
+					style="background-color:rgb(var(--v-theme-primary))"> OK </v-btn>
+				</v-card-actions>
+			</v-card>
+		</v-dialog>
+	</v-row>
+
 </template>
 
 <script lang="ts">
@@ -59,31 +101,29 @@
 
 <script setup lang="ts">
 /* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% */
-				/*  */
 import { info, info1, info2 , info3, info4, info5, info6, info7 } from "../my-util-code/MyConsoleUtil"
-import { enter, enter0, enter1, enter2, enter3, enter4, enter5, enter6, enter7 } from "../my-util-code/MyConsoleUtil"
-import { bar, whitebar, greybar, redbar, greenbar, orangebar } from "../my-util-code/MyConsoleUtil"
-import { log, warn, err, err2, exit, success, pass, fail, fini, start, progress, joy, } from "../my-util-code/MyConsoleUtil"
-				/*  */
+import { enter, enter0, enter1, 
+			bar, whitebar, greybar, 
+			log, warn, err } from "../my-util-code/MyConsoleUtil"
+/* ----------------------------------------------------------------------------- */
 import { ref, Ref } from 'vue'
 import { Auth } from 'aws-amplify';
+
+import { checkConfirmationTooShort, checkConfirmationSpecialChars,}
+	from "../components/ConfirmationParts/ComfirmationValidators"
 
 import { parseEmail, checkWorkingEmailSpecialChar, checkWorkingEmailName, checkWorkingEmailDomain } 
 	from "../components/EmailParts/EmailValidators"
 
 /* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% */
-const invalidEmailConfirmCode = ref("")
 const emailFormRef = ref()
-const confirmEmailCodeModel = ref("")
+
 const toggleConfirmEmail:Ref<boolean> = ref(false)
 const emailConfirmationMessage = { Title: ref(""), Message: ref(""), Message2: ref(""), Message3: ref("") }
 const workingEmailModel =ref("")
+
 const openDialogFlag = ref()
 const emit = defineEmits()
-/* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% */
-// defineProps({
-// 	isSession: Boolean,
-// })
 
 /* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% */
 /*																											*/
@@ -125,26 +165,31 @@ const resendEmailConfirmationCode = async () => {
 	const authUser = await Auth.currentAuthenticatedUser({bypassCache: true})
 	await Auth.updateUserAttributes(authUser, {'email': workingEmailModel.value });
 	confirmEmailCodeModel.value = ""
+	//					Clear all validation messages.
+	confirmEmailCodeModelFieldRef.value.resetValidation()
 }
 
 /* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% */
-const applyEmailConfirmationCode = async function () {
-	await Auth.verifyCurrentUserAttributeSubmit('email', `${ confirmEmailCodeModel.value}`)
-		.then((response) => {
-			// 		If we get here, the email is CONFIRMED.
+const applyEmailConfirmationCode = async function (event) {
+	const results = await event
+	if(!results.valid) return /* Cancel Submission if validation FAILED */
+	// if(BLOCKAPI("signUpUser function "))return
 
-			emit('onUpdateEmail', { email: workingEmailModel.value})
-			workingEmailModel.value = null
-			confirmEmailCodeModel.value = null
-			
-			toggleConfirmEmail.value = false
-		})
-		.catch((e) => {
-			openDialogFlag.value = true // open the Popup Dialog
-			confirmEmailCodeModel.value = null
-		})
-		return
-	}
+	await Auth.verifyCurrentUserAttributeSubmit('email', `${ confirmEmailCodeModel.value}`)
+	.then((response) => {
+		// 		If we get here, the email is CONFIRMED.
+
+		emit('onUpdateEmail', { email: workingEmailModel.value})
+		workingEmailModel.value = ""
+		confirmEmailCodeModel.value = ""
+		toggleConfirmEmail.value = false
+	})
+	.catch((e) => {
+		openDialogFlag.value = true // open the Popup Dialog
+		confirmEmailCodeModel.value = ""
+	})
+	return
+}
 	
 /* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% */
 const buildEmailConfirmationMessage = (email:string) => {
@@ -153,21 +198,24 @@ const buildEmailConfirmationMessage = (email:string) => {
 	emailConfirmationMessage.Message.value = 
 	`To confirm your new account, you must enter the ` +
 	`code we emailed to the new email address you provided.` 
-	
+
 	let {name , domain} = parseEmail(email)
+
 	emailConfirmationMessage.Message2.value = `<b>${name[0]}***@${domain[0]}***</b>`
-	
 	emailConfirmationMessage.Message3.value = `This may take a minuet to arrive.`
-	
 	return emailConfirmationMessage
 }
 
 /* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% */
 const clearWorkingEmailModelValidationError = () => emailFormRef.value.resetValidation()
 
+/* ----------------------------------------------------------------------------- */
+const confirmEmailCodeModel = ref("")
+const confirmEmailCodeModelFieldRef = ref()
+const clearConfirmEmailCodeModelValidationError = () => confirmEmailCodeModelFieldRef.value.resetValidation()
+
 /* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% */
 </script>
-
 
 <style>
 /* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% */

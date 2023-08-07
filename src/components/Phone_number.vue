59464a95120faba1944189d954a5517bcc296f37
kevinbowe <kevinbowe1957@gmail.com>
2023-08-07 11:48:36 -0400

    version -- v3-auth-vtfy3-v11 --------------------------------------------------
    Work-In-Progress -- WIP
    
    Import checkPhone_number(~) and checkPhone_numberInvalidCountryCode(~) from Phone_numerValidators.ts
    Discard the same validation code that was repeated in the component.
    
    --------------------------------------------------
    modified:    src/components/   Phone_number.vue
    --------------------------------------------------

--- src/components/Phone_number.vue
+++ src/components/Phone_number.vue
@@ -35,7 +35,9 @@
 <script setup lang="ts">
 /* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% */
 import {ref} from "vue"
-import { Auth } from 'aws-amplify';
+import { Auth } from "aws-amplify";
+import { checkPhone_number, checkPhone_numberInvalidCountryCode } 
+	from "../components/Phone_numberParts/Phone_numerValidators"
 
 
 /* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% */
@@ -49,59 +51,6 @@ const props = defineProps({
 const phone_numberModel = ref(props.p3)
 const workingPhone_numberModel = ref("")
 const resetPhone_number = () => { workingPhone_numberModel.value = phone_numberModel.value }
-
-/* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% */
-/* Phone number validation Decls */
-async function checkPhone_number (workingPhone_number) {
-	//				Strips these characters -- 'sp', '+', '-', '(', ')'
-	//				If a Country code is missing, add '1' (North America)
-	let strippedPhone_number = stripPhone_numberFmt(workingPhone_number)
-	//				Check length
-	if (strippedPhone_number.value.length < 11)
-	return `Incomplete phone number == char count == ${strippedPhone_number.value.length} == ${strippedPhone_number.value}`
-	//				Check for alpha characters
-	let expAtoZ = /[A-Za-z]/
-	let match = strippedPhone_number.value.match(expAtoZ)
-	if (match !== null )
-	return `Alphabetical characters are invalid == [${match}] == ${strippedPhone_number.value}`
-	//				Check for special characters
-	let expSpecChar = /[!@#$%\^&*(){}[\]<>?/|\\]/
-	let match2 = strippedPhone_number.value.match(expSpecChar)
-	if (match2 !== null)
-		return `Special characters are invalid == [${match2}] == ${strippedPhone_number.value}`
-		//			If we get here, the phone number passed all validation tests
-		return true
-	}
-/* ----------------------------------------------------------------------------- */
-/* Phone Number Filters */
-const InvalidCounteryCodes = {
-	7 : "Russia ",
-	53 : "Cuba" ,
-	591 : "Bolivia" ,
-	999 : "Unknown",
-	99 : "Unknown",
-	9 : "Unknown",
-	0 : "Unknown"
-}
-const countryCodeMap = new Map(Object.entries(InvalidCounteryCodes))
-async function checkPhone_numberInvalidCountryCode (workingPhone_number) {
-	//				Strips these characters -- 'sp', '+', '-', '(', ')'
-	//				If a Country code is missing, add '1' (North America)
-	const strippedPhone_number = stripPhone_numberFmt(workingPhone_number)
-	// 			Compare each value in the InvalidCounteryCodes array to the strippedPhone_number
-	let validationMessage:any = true
-	//				The copuntryCodeMap is based on a KVP object InvalidCountryCodes
-	for (const [key,value] of countryCodeMap)
-	{
-		const rx = new RegExp(`^${key}`, "g")
-		const match = strippedPhone_number.value.match(rx)
-		if (match !== null) {
-			validationMessage = `Invalid Country Code: ${key} [ ${value} ]`
-			break
-		}
-	}
-	return validationMessage
-}
 
 /* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% */
 /* Submit Phone Number Decl */

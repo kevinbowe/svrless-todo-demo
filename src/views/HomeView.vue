b7123bf2e6b35f673c2fb7048acc9f539aa9174a
kevinbowe <kevinbowe1957@gmail.com>
2023-08-11 12:07:27 -0400

    version -- v3-auth-vtfy3-v12 --------------------------------------------------
    Discard Obsolete <UserInfo> in Template.
    Modify label in Change Email Card.
    No functional change.
    
    --------------------------------------------------
    modified:    src/views/   HomeView.vue
    --------------------------------------------------

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -124,7 +124,7 @@
 						<v-card-text>
 							<v-row >
 								<v-col>
-									<v-row class="text-h6"> Change Email (conf)</v-row>
+									<v-row class="text-h6"> Change Email w/ conf </v-row>
 									<v-row>{{ emailModel }}</v-row>
 								</v-col>
 								<v-col align-self="center">
@@ -134,40 +134,7 @@
 						</v-card-text>
 					</v-card>
 				</div>
-					
-				<!-- <UserInfo/> -->
-				<!-- <v-row justify="center">
-					<v-col :lg="4" :md="6" :sm="8" :xs="12" class="ma-auto" >
-						<v-divider :thickness="10" class="ma-2"></v-divider>
-						
-						<v-row no-gutters>
-							<v-col style="background-color: rgb(var(--v-theme-surface)); color: rgb(var(--v-theme-border_alt));">
-								<p>Nick Name:</p></v-col>
-							<v-col><p>{{ nicknameModel }}</p></v-col>
-						</v-row>
-						
-						<v-divider :thickness="3" />
-						
-						<v-row no-gutters style="background-color: rgb(var(--v-theme-surface)); color: rgb(var(--v-theme-border_alt));">
-						<p class="ma-auto">Phone Number:</p></v-row>
-						<v-row no-gutters ><p class="ma-auto">{{ friendlyPhone(phone_numberModel) }}</p></v-row>
-							
-						<v-divider :thickness="3" />
-							
-						<v-row no-gutters style="background-color: rgb(var(--v-theme-surface)); color: rgb(var(--v-theme-border_alt));">
-						<p class="ma-auto">User Name:</p></v-row>
-						<v-row no-gutters ><p class="ma-auto">{{ usernameModel }}</p></v-row>
-								
-						<v-divider :thickness="3" />
-		
-						<v-row no-gutters style="background-color:rgb(var(--v-theme-surface)); color: rgb(var(--v-theme-border_alt));">
-						<p class="ma-auto">Email:</p></v-row>
-						<v-row no-gutters><p class="ma-auto">{{ emailModel }}</p></v-row>
-
-						<v-divider :thickness="10"></v-divider>
-					</v-col>
-				</v-row> -->
-						
+									
 				<!-- Sign Out -->
 				<SignOut @onSignOut="setSession"/>
 			</div>

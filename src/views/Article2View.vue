7edf66ebe509b6bf74fa429a95e042da36bf3b7b
kevinbowe <kevinbowe1957@gmail.com>
2023-08-14 10:46:06 -0400

    @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ @@                                                        @@ @@                   NEW PROJECT -- v13                   @@ @@                                                        @@ @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
    version -- v3-auth-vtfy3-v13
    --------------------------------------------------
    
    Big Clean Up -- Part 1
    Purge Obsolte files.
    
    --------------------------------------------------
    deleted:    src/components/Experiment_one.vue
    deleted:    src/components/Experiment_two.vue
    deleted:    src/components/HelloWorld.vue
    deleted:    src/components/Sandbox.vue
    deleted:    src/components/UserInfo.vue
    deleted:    src/views/Article2View.vue
    deleted:    src/views/Blog2View.vue
    deleted:    src/views/DevView1.vue
    deleted:    src/views/DevView2.vue
    deleted:    src/views/DevView3.vue
    deleted:    src/views/DevView4.vue
    deleted:    src/views/DevView5.vue
    deleted:    src/views/DevView6.vue
    deleted:    src/views/HomeView_Dialog.vue
    deleted:    src/views/SigninView.vue
    
    --------------------------------------------------

--- src/views/Article2View.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <v-container class="text-center">
-    <h1 class="text-primary">Article 2 Page Content</h1>
-		<v-btn class="pa-3" to="/">Go To Home</v-btn>
-  </v-container>
-</template>
-
-<script lang="ts"></script>
-<script lang="ts" setup></script>

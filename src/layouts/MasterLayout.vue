1b8407b14af972a6840739273b898489daf049a6
kevinbowe <kevinbowe1957@gmail.com>
2023-08-14 09:52:01 -0400

    @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ @@                                                        @@ @@                   NEW PROJECT -- v13                   @@ @@                                                        @@ @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
    version -- v3-auth-vtfy3-v13
    --------------------------------------------------
    
    Update App-Title-Bar -- No functional change.
    
    --------------------------------------------------
    modified:   src/layouts/MasterLayout.vue
    --------------------------------------------------

--- src/layouts/MasterLayout.vue
+++ src/layouts/MasterLayout.vue
@@ -223,7 +223,7 @@ import { ref } from "vue";
 import ThemeChanger from "../components/ThemeChanger.vue";
 import ThemePreview from "../components/ThemePreview.vue";
 // <v-app-bar-title>
-const mainTitle: string = "v3-Auth-Vtfy3-v12"
+const mainTitle: string = "v3-Auth-Vtfy3-v13"
 
 const menu = ref(false);
 const devLinks = ref([

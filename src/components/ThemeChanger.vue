1d91f90cc0bfed3d998d51d18b61d1924edba34c
kevinbowe <kevinbowe1957@gmail.com>
2023-09-06 09:49:01 -0400

    Version:   v3a-theme-pinia-v2 Date:      Wednesday, September 6, 2023 @ 9:49am -------------------------------------------------- Work-In-Progress -- WIP
    Improving DSK and MOB layout.
    
    --------------------------------------------------
    modified:   src/components/   ThemeChanger.vue
    --------------------------------------------------

--- src/components/ThemeChanger.vue
+++ src/components/ThemeChanger.vue
@@ -6,45 +6,44 @@
 			<v-btn class="mx-2" text="Factory Reset" color="secondary" @click="factoryThemeReset"/>
 			<v-btn text="Save New" class="mx-2" color="primary" @click="submitThemes"/>
 		</v-row>
-	
-		<v-row class="mb-5">
 
-			<v-btn width="16em" class="mr-3" rounded @click="toggleTheme(piniaStore.activeTheme)">
-				<div class="my-n5">
-					<v-icon size="60" color="green" v-if="piniaStore.activeTheme === theme.global.name.value" icon="mdi-toggle-switch"/>
-					<v-icon size="60" v-else color="red" icon="mdi-toggle-switch-off"/>
-					{{ piniaStore.activeTheme }}
-				</div>
-			</v-btn>
-			
-			<v-chip-group class="my-n1" v-model="leftSelection" >
+		<v-row >
+			<div class="float-md-left">
+				<v-btn width="16em" _class="float-md-left" class="mr-3" rounded @click="theme.global.name.value = piniaStore.activeTheme">
+					<div class="my-n5">
+						<v-icon size="60" color="green" v-if="piniaStore.activeTheme === theme.global.name.value" icon="mdi-toggle-switch"/>
+						<v-icon size="60" v-else color="red" icon="mdi-toggle-switch-off"/>
+						{{ piniaStore.activeTheme }}
+					</div>
+				</v-btn>
+			</div>
+			<div>
 				<v-chip v-for="theme in themeVals"
 				style="{ theme === piniaStore.activeTheme ? color:black;background-color: yellow; : color:black;background-color:grey;}"
-				@click="setLeft(theme)"> 
+				@click="setActiveTheme(theme)"> 
 					{{ theme }} 
 				</v-chip>
-			</v-chip-group>
-
+			</div>
 		</v-row>
-			
+
 		<v-row>
-			<v-btn width="16em" class="mr-3" rounded @click="toggleTheme(piniaStore.inactiveTheme)">
-				<div class="my-n5">
-					<v-icon size="60" color="green" v-if="piniaStore.inactiveTheme === theme.global.name.value" icon="mdi-toggle-switch"/>
-					<v-icon size="60" v-else color="red" icon="mdi-toggle-switch-off"/>
-					{{ piniaStore.inactiveTheme }}
-				</div>
-			</v-btn>
-		
-			<v-chip-group class="my-n1" v-model="rightSelection" >
+			<div class="float-md-left">
+				<v-btn width="16em" class="mr-3" rounded @click="theme.global.name.value = piniaStore.inactiveTheme">
+					<div class="my-n5">
+						<v-icon size="60" color="green" v-if="piniaStore.inactiveTheme === theme.global.name.value" icon="mdi-toggle-switch"/>
+						<v-icon size="60" v-else color="red" icon="mdi-toggle-switch-off"/>
+						{{ piniaStore.inactiveTheme }}
+					</div>
+				</v-btn>
+			</div>
+			<v-div>
 				<v-chip v-for="theme in themeVals"
 				style="{ theme === piniaStore.inactiveTheme ? color:black;background-color: yellow; : color:black;background-color:grey;}"
-				@click="setRight(theme)"> 
+				@click="setInactiveTheme(theme)"> 
 					{{ theme }} 
 				</v-chip>
-			</v-chip-group>
+			</v-div>
 		</v-row>
-
 	</v-container>
 
 	<v-container __MOBILE__ class="d-sm-none">
@@ -52,14 +51,9 @@
 	
 </template>
 
-<script lang="ts">
-	export default {inheritAttrs: false}
-</script>
-
 <script setup lang="ts">
 import { ref } from "vue";
 import { useTheme } from "vuetify";
-import StatusIcons from "./ThemeParts/StatusIcons.vue"
 import { Auth } from "aws-amplify";
 /* ----------------------------------------------------------------------------- */
 import {
@@ -74,101 +68,42 @@ const piniaStore = useUserPiniaStore(); // initialize the piniaStore
 
 const theme = useTheme();
 const themeVals = Object.keys(theme.computedThemes.value);
-const switchFlag = ref(false) // false == left // Active Theme
-const emit = defineEmits()
 
 // Set the default Models and Theme
 let leftModel:string = piniaStore.activeTheme
 let rightModel:string = piniaStore.inactiveTheme
 
 /* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% */
-const leftSelection = ref('light')
-const rightSelection = ref('dark')
 
-const setRight = (themeArg) => { 
-					enter(`setRight(theme) ${themeArg}`) 
+const setInactiveTheme = (themeArg) => { 
 	piniaStore.inactiveTheme = themeArg
-					info(`aT -> ${theme.global.name.value}`)
 	theme.global.name.value = themeArg
 }
 
-const setLeft = (themeArg) => { 
-					enter(`setLeft(themeArg) ${themeArg}`) 
+const setActiveTheme = (themeArg) => { 
 	piniaStore.activeTheme = themeArg
-					info(`aT -> ${theme.global.name.value}`)
 	theme.global.name.value = themeArg
 }
-
-const toggleTheme = (themeArg) => {
-	theme.global.name.value = themeArg
-}
-
 
 /* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% */
-async function submitThemes() {
-	// if(BLOCKAPI("submitThemes function ")) { return }
-
-	const custom_theme = ref(leftModel)
-	const custom_themeInactive = ref(rightModel)
-	
-	//					Which side is 'active' based on the switchFlag?
-	//						Switch left == false || Switch right == true
-	if(switchFlag.value){
-		custom_theme.value = rightModel
-		custom_themeInactive.value = leftModel
-	}
-
-	//					Update the active theme and piniaStores
-	piniaStore.activeTheme = custom_theme.value
-	piniaStore.inactiveTheme = custom_themeInactive.value
-	theme.global.name.value = custom_theme.value
-
+async function saveNew() {
 	//... if(BLOCKAPI("submitThemes function ")) { return }
 
 	//				This will return the user in the user pool (not updated )
-	Auth.currentAuthenticatedUser({bypassCache: true /* false */})
+	await Auth.currentAuthenticatedUser({bypassCache: true /* false */})
 	.then(user => {
 		Auth.updateUserAttributes(user, {
-		'custom:theme': custom_theme.value, 
-		'custom:theme-inactive': custom_themeInactive.value
-		})
-		info2(`Signed in -- Auth updated.`)
-	}).catch(reason => {
+		'custom:theme': piniaStore.activeTheme, 
+		'custom:theme-inactive': piniaStore.inactiveTheme
+	})
+	.then(result => {
+		loadSavedThemes()
+	})
+	.catch(reason => {
 					/** Not Signed in */
-					info(`Not signed in -- Auth not updated ${reason.message}`)
 		piniaStore.connected = false
 	})
-	//				FINDME FIXME
-	emit('onThemeChangerFini',  false )
-}
-
-/* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% */
-const onChangeSwitch = () => {
-	//					This will toggle the switch flag
-	switchFlag.value = !switchFlag.value;
-	//					This will change the display theme 
-	theme.global.name.value = 
-		theme.global.name.value === leftModel 
-			? rightModel 
-			: leftModel;
-};
-
-/* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% */
-//						This supports the LEFT and RIGHT v-selector
-
-function setClickSelectHandler( selectorModel: string  , selectorSwitchFlag: boolean ){
-
-					info(`selectorModel - > ${selectorModel}`)
-					info2(`selectorSwitchFlag - > ${selectorSwitchFlag}`)
-
-	// Update the right or left model depending on switchFlag
-	selectorSwitchFlag ? rightModel = selectorModel : leftModel = selectorModel 
-	//	Update the switch.
-	if(selectorSwitchFlag != switchFlag.value) switchFlag.value = !switchFlag.value;
-
-	// Update active theme
-	theme.global.name.value = selectorModel;
-}
+})}
 
 /* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% */
 const loadSavedThemes = async () => {
@@ -184,17 +119,8 @@ const loadSavedThemes = async () => {
 
 		piniaStore.authLightTheme = user.attributes['custom:theme']
 		piniaStore.authDarkTheme = user.attributes['custom:theme-inactive']
-	
 		//				Set the active theme
 		theme.global.name.value = user.attributes['custom:theme']
-		
-		//				Set the left and right selectors.
-
-			/**		CONTINUE WORKING HERE		*/
-
-
-
-
 	})
 	.catch((reason) => {
 	})
@@ -203,29 +129,30 @@ const loadSavedThemes = async () => {
 /* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% */
 const factoryThemeReset = async () => {
 
-	/**			DEBUG CODE ONLY 				*/
-	/**		Set the active theme.
-	 * 			Expect the theme to change
-	 * 		----------------------------------------------------
-	 * 		Set the left and right chip
-	 * 			The highlighted chip will be updated.
-	 * 		----------------------------------------------------		
-	 * 		The debug App-Bar will reflect the expected results.
-	 * 		----------------------------------------------------	*/
+	theme.global.name.value = 'light'
+	//				NOTE:
+	//				When piniaStore is updated the localStore will also be updated
+	piniaStore.activeTheme = 'light'
+	piniaStore.inactiveTheme = 'dark'
 
-	 //				WORKS
-	 theme.global.name.value = 'dark_custom'
+	piniaStore.authLightTheme = 'light'
+	piniaStore.authDarkTheme = 'dark'
 
-	 piniaStore.inactiveTheme = 'dark_custom'
-	 piniaStore.activeTheme = 'light_custom'
-
-	 leftSelection.value = 'left_custom'
-	 rightSelection.value = 'dark_custom'
-
-	 //if(BLOCKAPI("submitThemes function ")) { return }
+	if(BLOCKAPI("submitThemes function ")) { return }
 
 	//				This will return the user in the user pool (not updated )
-	// const user = await Auth.currentAuthenticatedUser({bypassCache: true /* false */});
+	Auth.currentAuthenticatedUser({bypassCache: true /* false */})
+	.then(user => {
+		Auth.updateUserAttributes(user, {
+			'custom:theme': 'light', 
+			'custom:theme-inactive': 'dark'
+		})
+	}).catch(reason => {
+					/** Not Signed in */
+	})
+	//				Auth.user DEBUG code
+	//				This will return the user in the user pool (not updated )
+	//	const user = await Auth.currentAuthenticatedUser({bypassCache: true /* false */});
 	//				This will delete the two attributes listed below.
 	// await Auth.deleteUserAttributes(user, ['custom:theme', 'custom:theme-inactive']);
 
@@ -280,7 +207,7 @@ const factoryThemeReset = async () => {
 }
 
 /* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% */
-/**/					const BLOCKAPIFLAG = ref(true)										 /**/
+/**/					const BLOCKAPIFLAG = ref(false)										 /**/
 /* 				if(BLOCKAPI("submitEmail function "))return								*/
 /* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% */
 const BLOCKAPI = (message:string|null|undefined = null) => {

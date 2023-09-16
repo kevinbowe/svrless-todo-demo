d6c776d246e9a649d773209376e31709c639ca03
kevinbowe <kevinbowe1957@gmail.com>
2023-09-16 13:26:53 -0400

    Version:   v3a-theme-pinia-v3 Date:      Saturday, September 16, 2023 @ 1:22 pm --------------------------------------------------
    Fix 6x typescript error/warnings in DeleteUser.vue
    Fix multiple typescript error/warnings in EmailValidator.vue.
       These were issues related to DeleteUser.vue.
       There are additional typescript error/warnings in EmailValidator.vue that have not been resolved yet.
    
    --------------------------------------------------
    modified:   src/components/              DeleteUser.vue
    modified:   src/components/EmailParts/   EmailValidators.ts
    --------------------------------------------------
    
    --------------------------------------------------

--- src/components/DeleteUser.vue
+++ src/components/DeleteUser.vue
@@ -54,11 +54,17 @@
 
 			<!-- Confirmation Message -->
 			<v-row class="pa-5" style="margin-top:-2.5em;" no-gutters>
+				<h1 class="ma-auto" v-html="deleteConfirmationMessage.Title"></h1>
+				<p v-html="deleteConfirmationMessage.Message"></p>
+				<p class="ma-auto" v-html="deleteConfirmationMessage.Message2"></p>
+				<p class="ma-auto" v-html="deleteConfirmationMessage.Message3"></p>
+			</v-row>
+			<!-- <v-row class="pa-5" style="margin-top:-2.5em;" no-gutters>
 				<h1 class="ma-auto" v-html="deleteConfirmationMessage.Title.value"></h1>
 				<p v-html="deleteConfirmationMessage.Message.value"></p>
 				<p class="ma-auto" v-html="deleteConfirmationMessage.Message2.value"></p>
 				<p class="ma-auto" v-html="deleteConfirmationMessage.Message3.value"></p>
-			</v-row>
+			</v-row> -->
 
 			<v-form validate-on="submit" @submit.prevent="applyDeleteConfirmationCode">
 				<v-text-field label="Confirmation Code" v-model="confirmDeleteCodeModel" 
@@ -117,8 +123,14 @@ const emit = defineEmits()
 /* ----------------------------------------------------------------------------- */
 const props = defineProps({
 	username: { type: String }, 
-	email: {type: String}
+	email: {type: String }
 })
+
+// /* ----------------------------------------------------------------------------- */
+// const props__OBS__ = defineProps({
+// 	username: { type: String, undefined }, 
+// 	email: {type: String, undefined}
+// })
 
 /* ----------------------------------------------------------------------------- */
 const confirmDeleteCodeModel = ref("")
@@ -126,22 +138,52 @@ const confirmDeleteCodeModelFieldRef = ref()
 const clearConfirmDeleteCodeModelValidationError = () => confirmDeleteCodeModelFieldRef.value.resetValidation()
 /* ----------------------------------------------------------------------------- */
 const toggleConfirmDelete:Ref<boolean> = ref(false)
-const deleteConfirmationMessage = { Title: ref(""), Message: ref(""), Message2: ref(""), Message3: ref("") }
-const openDialogFlag = ref()
+// const deleteConfirmationMessage = { Title: ref(""), Message: ref(""), Message2: ref(""), Message3: ref("") }
 
+type  deleteConfirmationMessageType = { Title: string, Message: string, Message2: string, Message3: string }
+const deleteConfirmationMessage: deleteConfirmationMessageType = { Title: "", Message: "", Message2: "", Message3: "" }
+
+const openDialogFlag = ref()
 const showDeleteUser = ref(false)
 
 /* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% */
 const sendDeleteUser = async () => {
 	if(BLOCKAPI("sendDeleteUser function ")) { 
-		deleteConfirmationMessage.value = buildDeleteConfirmationMessage(props.email)
+
+		const rtn = buildDeleteConfirmationMessage(props.email)
+		deleteConfirmationMessage.Message = rtn.Message
+		deleteConfirmationMessage.Message2 = rtn.Message2
+		deleteConfirmationMessage.Message3 = rtn.Message3
+		deleteConfirmationMessage.Title = rtn.Title
+
+		// deleteConfirmationMessage = buildDeleteConfirmationMessage(props.email)
+
+		// deleteConfirmationMessage.value = buildDeleteConfirmationMessage(props.email)
 		toggleConfirmDelete.value = true; 
+
+		info(`Message.Title \n      ${deleteConfirmationMessage.Title}`)
+		info(`Message.Message \n      ${deleteConfirmationMessage.Message}`)
+		info(`Message.Message2 \n      ${deleteConfirmationMessage.Message2}`)
+		info(`Message.Message3 \n      ${deleteConfirmationMessage.Message3}`)
+		// info(`Message.Title.value \n      ${deleteConfirmationMessage.Title.value}`)
+		// info(`Message.Message.value \n      ${deleteConfirmationMessage.Message.value}`)
+		// info(`Message.Message2.value \n      ${deleteConfirmationMessage.Message2.value}`)
+		// info(`Message.Message3.value \n      ${deleteConfirmationMessage.Message3.value}`)
 		return 
 	}
 
 	await Auth.verifyCurrentUserAttribute("email")
 	.then(response => { 
-		deleteConfirmationMessage.value = buildDeleteConfirmationMessage(props.email)
+		// deleteConfirmationMessage.value = buildDeleteConfirmationMessage(props.email)
+
+		const rtn = buildDeleteConfirmationMessage(props.email)
+		deleteConfirmationMessage.Message = rtn.Message
+		deleteConfirmationMessage.Message2 = rtn.Message2
+		deleteConfirmationMessage.Message3 = rtn.Message3
+		deleteConfirmationMessage.Title = rtn.Title
+
+
+
 		toggleConfirmDelete.value = true 
 	})
 	.catch(e => err(`Auth.verifyCurrentUserAttribute FAILED ${e}`))
@@ -183,19 +225,22 @@ const resendDeleteConfirmationCode = async () => {
 }
 
 /* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% */
-const buildDeleteConfirmationMessage = (email:string) => {
-	deleteConfirmationMessage.Title.value = "Last Chance..."
-	deleteConfirmationMessage.Message.value = 
+const buildDeleteConfirmationMessage = (email:string | undefined) => {
+	deleteConfirmationMessage.Title = "Last Chance..."
+	deleteConfirmationMessage.Message = 
 	`Before we delete your account, you must enter the ` +
 	`code we sent to the email address associated with this account.` 
 
-	let {name , domain} = parseEmail(email)
-	deleteConfirmationMessage.Message2.value = `<b>${name[0]}***@${domain[0]}***</b>`
-	deleteConfirmationMessage.Message3.value = `It may take a minuet to arrive.`
-
+	type parseEmailType = {name:string|undefined, domain:string | undefined} | null
+	let nameDomain: parseEmailType = parseEmail(email)
+	
+	deleteConfirmationMessage.Message2 = `<div style="height:4em;"></div>`
+	if(nameDomain && nameDomain?.name && nameDomain?.domain) {
+		deleteConfirmationMessage.Message2 = `<b>${nameDomain?.name[0]}***@${nameDomain?.domain[0]}***</b>`
+	}
+	deleteConfirmationMessage.Message3 = `It may take a minuet to arrive.`
 	return deleteConfirmationMessage
 }
-
 
 /* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% */
 /* 				if(BLOCKAPI("submitEmail function "))return								*/
--- src/components/EmailParts/EmailValidators.ts
+++ src/components/EmailParts/EmailValidators.ts
@@ -1,13 +1,15 @@
 import { info, info1, info2 , info3, info4, info5, info6, info7 } from "../../my-util-code/MyConsoleUtil"
 import { enter, enter0, enter1, enter2, enter3, enter4, enter5, enter6, enter7 } from "../../my-util-code/MyConsoleUtil"
 import { bar, whitebar, greybar, redbar, greenbar, orangebar } from "../../my-util-code/MyConsoleUtil"
-
+ 
 /* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% */
-export const parseEmail = (email) => {
+export const parseEmail = (email) : { name: string|undefined, domain: string|undefined } | null => {
 	if (!(!!email)) return null
 	const regex = new RegExp('^(?<name>.*)@(?<domain>.*)', 'gm')
 	let match = regex.exec(email)
-	if (match) return { name: match.groups.name, domain: match.groups.domain }
+
+	if (match) 
+		return { name: match.groups?.name, domain: match.groups?.domain }
 	return null
 }
 

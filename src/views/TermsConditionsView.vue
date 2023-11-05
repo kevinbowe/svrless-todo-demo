e5dfbadb5f16f4f1ab1333491af5c2e733c6c4b5
kevinbowe <kevinbowe1957@gmail.com>
2023-11-05 11:44:57 -0500

    -------------------------------------------------- Version:   v3a-todo-theme-v1 Date:      Sunday, November 5, 2023 @ 11:41 am --------------------------------------------------
    Redo Terms and Conditions page.
       Replace the content
       Replace the layout
    
    --------------------------------------------------
    modified:   src/views/   TermsConditionsView.vue
    --------------------------------------------------

--- src/views/TermsConditionsView.vue
+++ src/views/TermsConditionsView.vue
@@ -1,38 +1,35 @@
 <template>
-<!-- <v-app> -->
+<v-app>
 <MasterLayout>
-	<v-container class="text-align:start">
-		<h1 style="text-align: center;">Terms and Conditions</h1>
-		<v-row>
-			<v-spacer></v-spacer>
-			<v-col cols="4" style="text-align: center;">
-					<p>www.DaBowe.com</p>
-					221a Baker Street, 
-					<p>Durham, North Carolina 27703</p>
-					<p>919 333-4444</p>
-			</v-col>
-			<v-spacer></v-spacer>
-		</v-row>
+	<v-col class="d-flex justify-center">
+		<v-card width="60%" color="secondary" variant="tonal" class="text-h4">Terms and Conditions</v-card>
+	</v-col>
+	
+	<p class="my-5"> www.DaBowe.com</p>
 
-		<h2 class="my-5" style="text-align: center;">Last Updated: May 19, 2023</h2>
+	<p>828 Glenco Rd,</p>
+	<p>Durham, North Carolina 27703</p>
+	<p>919 272-7866</p>
 
-		<v-row>
-			<v-spacer/>
-			<v-col cols="12" style="text-align: start;">
-				<h1>Introduction</h1>
+	<p class="my-5">Kevin Bowe (KB)</p>
+	<p class="my-5 text-h5">Last Updated: May 19, 2023</p>
+
+	<v-container>
+			<v-row style="text-align:left;">
+				<p class="mb-5 text-h4">Introduction</p>
 				<p class="mx-10">These Terms and Conditions... Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			</v-row>
 
-				<h1 class="my-5">Terms</h1>
-
-				<h3 class="mb-5">Registration, Access and User.</h3>
+			<v-row class="my-10 text-h4">Terms</v-row>
+			
+			<v-row style="text-align:left;">
+				<p class="mb-5 text-h6">Registration, Access and User.</p>
 				<p class="mx-10">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-			</v-col>
-			<v-spacer/>
-		</v-row>		
+			</v-row>
 	</v-container>
+
 </MasterLayout>
-<!-- </v-app> -->
+</v-app>
 </template>
 
 <script lang="ts" setup>

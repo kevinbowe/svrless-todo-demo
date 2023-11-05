5cb33b4d6d7f74b5dc1c6ed93f39f6e66b27fe14
kevinbowe <kevinbowe1957@gmail.com>
2023-11-05 12:05:05 -0500

    -------------------------------------------------- Version:   v3a-todo-theme-v1 Date:      Sunday, November 5, 2023 @ 11:59 am --------------------------------------------------
    Add Hero.svg to assets.
    Discard everything in the <template>
    Create new <template> with image, copy and button.
    
    --------------------------------------------------
    modified.   src/assets/   Hero.svg
    modified:   src/views/    HomeView.vue
    --------------------------------------------------

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,26 +1,20 @@
-<!-- src/views/HomeView.vue -->
- 
 <script lang="ts">
-	const showDev = ref(false)
 	const BLOCKAPIFLAG = ref(false)
 </script>
 
 <template>
 	<v-app>
 		<MasterLayout>
-
-			<v-row v-if="showDev">
-				<v-spacer/>
-				<v-col cols="2">
-					<v-btn color="grey-darken-2" class="ma-1" @click="piniaStore.deleteAllDevStores()">Delete Dev LS</v-btn>
-				</v-col>
-				<v-col cols="3">
-					<v-btn color="grey-darken-2" class="ma-1" @click="piniaStore.clearAllStores()">Delete ( Dev & Auth ) LS</v-btn>
-					This will Sign Out User
-				</v-col>
-				<v-spacer/>
-			</v-row>
-
+			<v-container class="d-flex flex-wrap my-n2" >
+				<v-row align="center" no-gutters class="mt-n5">
+					<v-col><img :height="mobile? 280:600" src="../assets/hero.svg"/></v-col>
+					<v-col>
+						<div :class="mobile? 'text-h4':'text-h3'">Welcome to Ohggi</div>
+						<div>by DaBowe.com</div>
+						<v-btn to="/todo" color="primary" text="select to continue" class="mt-4"></v-btn>
+					</v-col>
+				</v-row>
+			</v-container>
 		</MasterLayout>
 	</v-app>
 </template>
@@ -32,17 +26,15 @@ import { bluebar, info, info1, info2 , info3, info4, info5, info6, info7 } from 
 import { enter, enter0, enter1, enter2, enter3, enter4, enter5, enter6, enter7 } from "../my-util-code/MyConsoleUtil"
 import { bar, whitebar, greybar, redbar, greenbar, orangebar } from "../my-util-code/MyConsoleUtil"
 /* ----------------------------------------------------------------------------- */
-import { useTheme } from "vuetify";
+import { useTheme, useDisplay } from "vuetify";
 import { useUserPiniaStore } from "../stores/user"
 import  ThemeChanger from "../components/ThemeChanger.vue"
 import { ref } from "vue";
 
-//import colors from 'vuetify/lib/util/colors'
-
 /* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% */
-// const { mobile } = useDisplay()
-const piniaStore = useUserPiniaStore(); // initialize the piniaStore
+const piniaStore = useUserPiniaStore();
 const theme = useTheme();
+const { mobile } = useDisplay()
 
 /* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% */
 /* 				if(BLOCKAPI("submitEmail function "))return								*/

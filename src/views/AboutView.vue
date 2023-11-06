c5d12d333e32501e47cdccfacd29459edc348de8
kevinbowe <kevinbowe1957@gmail.com>
2023-11-06 10:29:14 -0500

    -------------------------------------------------- Version:   v3a-todo-theme-v1 Date:      Monday, November 6, 2023 @ 10:27 am --------------------------------------------------
    Modify the Title card at the top of the page.
       Now it is responsive.
    Proof-read and edit content.
    Discard OBS code.
    
    --------------------------------------------------
    modified:   src/views/   AboutView.vue
    
    --------------------------------------------------

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,9 +1,11 @@
 <template><v-app><MasterLayout>
-
-<v-col class="d-flex justify-center">
-	<v-card width="60%" color="secondary" variant="tonal" class="text-h4 mb-5">About Me</v-card>
-</v-col>
-
+<v-row>
+	<v-spacer/>
+	<v-col _xs="12" sm="8" md="5" lg="4" class="d-flex justify-center">
+		<v-card width="100%" color="secondary" variant="tonal" class="text-h4 mb-5">About</v-card>
+	</v-col>
+	<v-spacer/>
+</v-row>
 <v-row justify="center">
 	<v-col sm="8" md="5" lg="4">
 		<v-card>
@@ -40,70 +42,38 @@
 	<v-col sm="8" md="5" lg="4">
 		
 		<v-row class="my-5 text-h6" style="line-height: 1.6;text-align:left;">
-			Athletes, artists, warriors and individuals with master level skills all have one thing in common:
+			Athletes, artists, warriors and individuals with master level skills, all have one thing in common:
 		</v-row>
 
-		<span class="text-h5 text-secondary">They all practice.</span>
+		<span class="text-h5 text-secondary">They practice.</span>
 
 		<v-row class="my-5 text-h6" style="line-height: 1.6;text-align:left;">
-			They train endlessly. They repeat the skills they have been taught until they have reached their peak personal performance. They continue to train to maintain their peak performance.
+			They train endlessly. They repeat the skills they have been taught until they have reached their personal best. They continue to train to maintain their personal best.
 		</v-row>
 
 		<span class="text-h5 text-secondary">Do you train?</span>
 
 		<v-row class="my-5 text-h6" style="line-height: 1.6;text-align:left;">
-			Are you performing the skills you have learned at your peak performance?
+			Are you performing the skills you have learned, at your personal best?
 		</v-row>
 
 		<span class="text-h5 text-secondary">Probably not.</span>
 
 		<v-row class="my-5 text-h6" style="line-height: 1.6;text-align:left;">
-			Do you even know how to train for the skills you have?
+			Do you know what skills you should be targeting?
+			Do you know how to train on those skills?
 		</v-row>
 
 		<span class="my-5 text-h5 text-secondary">Probably not.</span>
 
 		<div class="my-5 text-h6">What are you going to do about it? </div>
 
-
-		<span class="text-h5 text-secondary">Well?</span>
-
-		<div class="d-flex justify-center my-5 mx-n3  text-h6" style="line-height: 1.6;text-align:left;" >
+		<!-- <div class="d-flex justify-center my-5 mx-n3  text-h6" style="line-height: 1.6;text-align:left;" >
 			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-		</div>
+		</div> -->
 	</v-col>
 </v-row>
 
-
-
-
-
-
-
-
-	<!-- <v-row justify="center" class="mt-10" >
-		<v-divider></v-divider>
-		<v-col sm="5" >
-			<v-card _style="text-align:left;">
-				<v-card-title class="bg-cyan-darken-1 " >
-					<span class="white-text">Da Bowe</span>
-				</v-card-title>
-				<v-list>
-					<v-list-item 
-					prepend-avatar="./src/assets/Beard_DaBowe.svg" 
-					subtitle="Mr Bowe if yo' nasty">
-						<v-btn 
-						variant="text" 
-						:class="fav ? 'text-red' : ''" 
-						icon="mdi-heart" 
-						@click="fav = !fav">
-						</v-btn>
-					</v-list-item>
-				</v-list>
-			</v-card>
-		</v-col>
-	</v-row> -->
-	
 </MasterLayout></v-app></template>
 
 <script lang="ts" setup>

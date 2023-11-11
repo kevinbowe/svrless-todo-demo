f9b5c30def9f522146ef1153ab1eef063ce34f68
kevinbowe <kevinbowe1957@gmail.com>
2023-11-11 15:37:57 -0500

    -------------------------------------------------- Version:   v3a-todo-theme-v1 Date:      Saturday, November 11, 2023 @ 3:17 pm --------------------------------------------------
    Import all of the MyConsoleUtil.ts utilities to help with debugging.
    
    Discarded the surface_alt and border_alt colorFilters
      The colors are still available.
      They will not be displayed in the ThemeChanger.
    
    Add comments that describe the code.
    
    --------------------------------------------------
    modified:   src/components/   ThemePreview.vue
    --------------------------------------------------

--- src/components/ThemePreview.vue
+++ src/components/ThemePreview.vue
@@ -56,12 +56,31 @@
 
 <script setup lang="ts">
 import { useTheme } from "vuetify";
+/* ----------------------------------------------------------------------------- */
+import { bluebar, info, info1, info2 , info3, info4, info5, info6, info7, infor, pass, fail }
+	from "../my-util-code/MyConsoleUtil" // Cant find this in Prod Build
 
+import { enter, enter0, enter1, enter2, enter3, enter4, enter5, enter6, enter7 }
+	from "../my-util-code/MyConsoleUtil" // Cant find this in Prod Build
+
+import { bar, whitebar, greybar, redbar, greenbar, orangebar }
+	from "../my-util-code/MyConsoleUtil" // Cant find this in Prod Build
+
+import { log, warn, err2, err, progress, joy, infob, infog, infoy, infoo, infop,
+		infom, exit, success, start, fini, pause, resume,
+		exe, exe1, exe2, exe3, exe4, exe5, exe6, exe7, }
+	from "../my-util-code/MyConsoleUtil" // Cant find this in Prod Build
+/* -------------------------------------------------------------------------- */
 const uTheme = useTheme();
 const computedThemesKeysValue = Object.keys(uTheme.computedThemes.value);
 const computedThemesEntriesValue = Object.entries(uTheme.computedThemes.value);
 const chipVariant: any = "outlined";
-const colorFilter = ['background','surface','primary','secondary','info','error','success','warning','important','minor', 'major','link', 'surface_alt', 'border','border_alt']
+
+const colorFilter = [
+	'background','surface','primary','secondary',
+	'info','error','success','warning','important',
+	'minor', 'major','link', 'border']
+
 const themeLength = computedThemesKeysValue.length;
 
 type colorArrayType = { color: string; code: string }
@@ -71,22 +90,32 @@ type themeObjsType = { theme: string, colorArray: colorArrayType[] }
 const generateThemesWithAllColors = () => {
 	let themeObjs: themeObjsType[] = [];
 	let colorArray: colorArrayType [] = [];
+	
 	// Create array to store the themes and related VideoColorSpace
 	for (let i = 0; i < themeLength; i++) {
-		// Fetch the Theme name
+
+		// 			Fetch the Theme name
+		//				This will return [ light | dark | light_custom | dark_custom ]
 		let themeName = computedThemesKeysValue[i];
-		// Fetch the Color elements
+
+		//				This will return the properties I set/created AND other properties
+		//				like: surface_variant, on-surface_variant, 
 		let themeColorProps = computedThemesEntriesValue[i][1].colors;
-		//	Look for matching colors
+
+		//				Look for matching colors
+		//				This builds a list of matches in the colorFilter.
 		let matches = Object.entries(themeColorProps).filter(val => colorFilter.includes(val[0]) )
+
 		//	Hydrate the colorArray with matching colors
 		matches.forEach(match => colorArray.push( {color: match[0], code: match[1]} ) )
-		// create a themeObject
+
+		// 			Create a themeObject
 		let themeObj: themeObjsType = { theme: themeName, colorArray: colorArray };
 		themeObjs.push(themeObj);
-		// Clear the colorArray for the next theme
+		// 			Clear the colorArray for the next theme
 		colorArray = [];
 	}
+	// whitebar()
 	return themeObjs;
 };
 
